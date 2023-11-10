7dbbd868f5a225d9c463daa613f1036fa974da29
dilaouid <dilaouid@student.42.fr>
2023-11-10 18:43:14 +0100

    (~) import plugins to an index for export

--- backend/src/plugins/index.ts
+++ backend/src/plugins/index.ts
@@ -1,7 +1,7 @@
-import { corsPlugin } from "./cors";
-import { logger } from "./logger";
-import { websocketPlugin } from "./websocket";
-import { envPlugin } from "./env";
+import { corsPlugin } from "./cors.plugin";
+import { logger } from "./logger.plugin";
+import { websocketPlugin } from "./websocket.plugin";
+import { envPlugin } from "./env.plugin";
 
 export {
     logger,
