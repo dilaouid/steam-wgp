420dbf94f9b4e624667581b6aec43fe5da5c3b27
dilaouid <dilaouid@student.42.fr>
2023-11-11 10:57:52 +0100

    (~) fix pretty logger

--- backend/src/plugins/logger.plugin.ts
+++ backend/src/plugins/logger.plugin.ts
@@ -1,6 +1,7 @@
 import pino from 'pino';
 
 export const logger = pino({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   transport: {
     target: 'pino-pretty',
     options: {
