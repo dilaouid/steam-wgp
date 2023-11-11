12f6ce4827e560ab07a1fe3021461c73be1ef6f2
dilaouid <dilaouid@student.42.fr>
2023-11-11 22:04:11 +0100

    (+) add NODE_ENV env var

--- backend/src/plugins/env.plugin.ts
+++ backend/src/plugins/env.plugin.ts
@@ -1,35 +1,39 @@
 import fastifyEnv from '@fastify/env';
 
 const schema = {
-    type: 'object',
-    required: ['PORT', 'STEAM_API_KEY'],
-    properties: {
-        HOST: {
-            type: 'string',
-            default: 'localhost'
-        },
-        PORT: {
-            type: 'string',
-            default: '8080'
-        },
-        ORIGIN: {
-            type: 'string',
-            default: 'http://localhost:3000'
-        },
-        STEAM_API_KEY: {
-            type: 'string'
-        },
-        DATABASE_URL: {
-            type: 'string',
-            default: 'postgres://postgres:postgres@localhost:5432/steam'
-        }
+  type: 'object',
+  required: ['PORT', 'STEAM_API_KEY'],
+  properties: {
+    HOST: {
+      type: 'string',
+      default: 'localhost'
+    },
+    PORT: {
+      type: 'string',
+      default: '8080'
+    },
+    ORIGIN: {
+      type: 'string',
+      default: 'http://localhost:3000'
+    },
+    STEAM_API_KEY: {
+      type: 'string'
+    },
+    DATABASE_URL: {
+      type: 'string',
+      default: 'postgres://postgres:postgres@localhost:5432/steam'
+    },
+    NODE_ENV: {
+      type: 'string',
+      default: 'development',
     }
+  }
 };
 
 export const envPlugin = async (fastify: any) => {
-    return fastify.register(fastifyEnv, {
-        confKey: 'config',
-        schema,
-        dotenv: true
-    });
+  return fastify.register(fastifyEnv, {
+    confKey: 'config',
+    schema,
+    dotenv: true
+  });
 };
