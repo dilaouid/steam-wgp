ef4e1c9eccc8af2046a1a28c0efc1562fa146cd5
dilaouid <dilaouid@student.42.fr>
2023-11-14 18:32:17 +0100

    (~) fix types and default value for waitlists table

--- backend/src/models/Waitlists.ts
+++ backend/src/models/Waitlists.ts
@@ -1,10 +1,12 @@
-import { pgTable, uuid, text, integer, boolean, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, bigserial, boolean, timestamp } from "drizzle-orm/pg-core";
 import { model as players } from "./Players";
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { FastifyInstance } from "fastify";
+import { hashGenerator } from "../utils/hash";
 
 export const model = pgTable('waitlists', {
   id: uuid('id').primaryKey(),
-  hash: text('hash').notNull().unique(),
+  hash: text('hash').notNull().unique().default(hashGenerator(2)),
   admin_id: bigserial('admin_id', { mode: 'bigint' }).references(() => players.id),
   started: boolean('started').default(false),
   created_at: timestamp('created_at').notNull().defaultNow(),
@@ -12,4 +14,9 @@ export const model = pgTable('waitlists', {
 });
 
 export type Waitlist = InferSelectModel<typeof model>;
-export type WaitlistInsert = InferInsertModel<typeof model>;
+export type WaitlistInsert = Omit<InferInsertModel<typeof model>, 'id' | 'created_at' | 'updated_at'> & {
+  id?: string;
+  hash?: string;
+  created_at?: Date;
+  updated_at?: Date;
+};
