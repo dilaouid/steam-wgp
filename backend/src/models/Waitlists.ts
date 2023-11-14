a958ceebd7a4402e7ed2d3124621246ce6681132
dilaouid <dilaouid@student.42.fr>
2023-11-14 18:23:25 +0100

    (~) change admin_id to bigserial type

--- backend/src/models/Waitlists.ts
+++ backend/src/models/Waitlists.ts
@@ -5,7 +5,7 @@ import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 export const model = pgTable('waitlists', {
   id: uuid('id').primaryKey(),
   hash: text('hash').notNull().unique(),
-  admin_id: integer('admin_id').references(() => players.id),
+  admin_id: bigserial('admin_id', { mode: 'bigint' }).references(() => players.id),
   started: boolean('started').default(false),
   created_at: timestamp('created_at').notNull().defaultNow(),
   updated_at: timestamp('updated_at').notNull().defaultNow()
