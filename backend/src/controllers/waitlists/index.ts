d1df801416a1628404314c3ac5f173dedc7a4f75
dilaouid <dilaouid@student.42.fr>
2023-11-15 11:11:29 +0100

    (+) barrel export waitlists controllers

---
+++ backend/src/controllers/waitlists/index.ts
@@ -0,0 +1,11 @@
+import { createWaitlist } from "./createWaitlistRoom";
+import { getWaitlistWithPlayers, getWaitlistWithPlayersParams } from "./getWaitlistRoom";
+import { joinOrLeaveWaitlist, joinOrLeaveWaitlistParams } from "./joinWaitlistRoom";
+
+export {
+  createWaitlist,
+  getWaitlistWithPlayers,
+  joinOrLeaveWaitlist,
+  joinOrLeaveWaitlistParams,
+  getWaitlistWithPlayersParams
+}
