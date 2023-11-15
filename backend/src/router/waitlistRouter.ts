0debb5ea8c3c37da22702c7f8e46c59341bb6500
dilaouid <dilaouid@student.42.fr>
2023-11-15 11:42:01 +0100

    (+) create waitlist router

---
+++ backend/src/router/waitlistRouter.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from 'fastify';
+import { createWaitlist } from '../controllers/waitlists';
+import { isAuthenticated } from '../auth/mw';
+
+import {
+  getWaitlistWithPlayers as getWaitlist,
+  getWaitlistWithPlayersOpts as getOpts,
+  getWaitlistWithPlayersParams as getParams } from '../controllers/waitlists';
+
+import {
+  joinOrLeaveWaitlist as joinOrLeave,
+  joinOrLeaveWaitlistParams as joinParams } from '../controllers/waitlists';
+
+export default async function waitlistRouter(fastify: FastifyInstance) {
+  fastify.register(async function (fastify) {
+    fastify.post('/', { preValidation: [isAuthenticated] }, createWaitlist);
+    fastify.get<{ Params: getParams }>('/:hash', getOpts, getWaitlist);
+    fastify.patch<{ Params: joinParams }>('/:id', { preValidation: [isAuthenticated] }, joinOrLeave);
+  }, { prefix: '/waitlist' });
+}
