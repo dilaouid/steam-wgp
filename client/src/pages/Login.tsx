f6269eaeb03a3de2949f78c6a8a28e4e17d6ac5e
dilaouid <dilaouid@student.42.fr>
2023-11-25 22:19:30 +0100

    (+) init router and login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import HeadingContainerComponent from '../components/common/Login/HeadingContainer';
+
+export default function LoginPage() {
+    return (
+        <section className="py-4 py-xl-5">
+            <div className="container">
+                <HeadingContainerComponent />
+            </div>
+        </section>
+    );
+}
