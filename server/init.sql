f1727e0e06ad07f47ee72aa6cd1be305fa167375
dilaouid <dilaouid@student.42.fr>
2023-11-26 17:32:29 +0100

    (~) fix init.sql

--- server/init.sql
+++ server/init.sql
@@ -1,34 +1,34 @@
 -- Création de la table games
-CREATE TABLE IF NOT EXISTS games (
+CREATE TABLE games (
     id SERIAL PRIMARY KEY,
-    is_selectable BOOLEAN NOT NULL
+    is_selectable BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Création de la table players
-CREATE TABLE IF NOT EXISTS players (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE players (
+    id BIGINT PRIMARY KEY,
     avatar_hash VARCHAR(255) NOT NULL
 );
 
 -- Création de la table libraries
-CREATE TABLE IF NOT EXISTS libraries (
-    id UUID PRIMARY KEY,
-    player_id INT REFERENCES players(id),
-    game_id INT REFERENCES games(id)
+CREATE TABLE libraries (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- Assurez-vous que l'extension pgcrypto est installée pour gen_random_uuid()
+    player_id BIGINT REFERENCES players(id) ON DELETE CASCADE,
+    game_id INT REFERENCES games(id) ON DELETE CASCADE
 );
 
 -- Création de la table waitlists
-CREATE TABLE IF NOT EXISTS waitlists (
-    id VARCHAR(60) PRIMARY KEY,
-    admin_id INT REFERENCES players(id),
-    started BOOLEAN NOT NULL,
-    created_at DATE NOT NULL,
-    updated_at DATE NOT NULL
+CREATE TABLE waitlists (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- Utiliser UUID pour l'ID
+    admin_id BIGINT REFERENCES players(id) ON DELETE SET NULL,
+    started BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Création de la table waitlists_players
-CREATE TABLE IF NOT EXISTS waitlists_players (
-    player_id INT REFERENCES players(id),
-    waitlist_id UUID REFERENCES waitlists(id),
+CREATE TABLE waitlists_players (
+    player_id BIGINT REFERENCES players(id) ON DELETE CASCADE,
+    waitlist_id UUID REFERENCES waitlists(id) ON DELETE CASCADE,
     PRIMARY KEY (player_id, waitlist_id)
 );
