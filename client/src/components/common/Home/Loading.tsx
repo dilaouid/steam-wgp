0a0021551df3ae86b4dbda41de04f5e65cc87b90
dilaouid <dilaouid@student.42.fr>
2023-11-29 21:45:58 +0100

    (+) Steam loading spinner

---
+++ client/src/components/common/Home/Loading.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Spinner = styled.span`
+    margin-top: 22px;
+    width: 140px;
+    height: 146px;
+    box-shadow: 0px 0px 20px 5px;
+    margin-bottom: 15px;
+`;
+
+const SteamIcon = styled.svg`
+    position: absolute;
+    margin: 18px;
+    margin-top: 42px;
+    font-size: 106px;
+`;
+
+export default function SteamLoadingIcon() {
+    return (
+        <div data-aos="zoom-in" data-aos-duration="1000">
+            <SteamIcon xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-steam text-info">
+                <path d="M.329 10.333A8.01 8.01 0 0 0 7.99 16C12.414 16 16 12.418 16 8s-3.586-8-8.009-8A8.006 8.006 0 0 0 0 7.468l.003.006 4.304 1.769A2.198 2.198 0 0 1 5.62 8.88l1.96-2.844-.001-.04a3.046 3.046 0 0 1 3.042-3.043 3.046 3.046 0 0 1 3.042 3.043 3.047 3.047 0 0 1-3.111 3.044l-2.804 2a2.223 2.223 0 0 1-3.075 2.11 2.217 2.217 0 0 1-1.312-1.568L.33 10.333Z"></path>
+                <path d="M4.868 12.683a1.715 1.715 0 0 0 1.318-3.165 1.705 1.705 0 0 0-1.263-.02l1.023.424a1.261 1.261 0 1 1-.97 2.33l-.99-.41a1.7 1.7 0 0 0 .882.84Zm3.726-6.687a2.03 2.03 0 0 0 2.027 2.029 2.03 2.03 0 0 0 2.027-2.029 2.03 2.03 0 0 0-2.027-2.027 2.03 2.03 0 0 0-2.027 2.027Zm2.03-1.527a1.524 1.524 0 1 1-.002 3.048 1.524 1.524 0 0 1 .002-3.048Z"></path>
+            </SteamIcon>
+            <Spinner className="spinner-border text-info-emphasis" role="status" />
+        </div>
+    );
+}
--- client/src/components/common/Login/Disclaimer.tsx
+++ client/src/components/common/Login/Disclaimer.tsx
@@ -1,4 +1,3 @@
-
 import styled from 'styled-components';
 
 const Disclaimer = styled.p`
