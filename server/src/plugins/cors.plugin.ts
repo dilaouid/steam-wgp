3a11538f063dfac871744f595f62af230a6aa5f6
dilaouid <dilaouid@student.42.fr>
2023-11-29 21:45:11 +0100

    (+) improve fastify cors plugin

--- server/src/plugins/cors.plugin.ts
+++ server/src/plugins/cors.plugin.ts
@@ -1,9 +1,14 @@
 import { fastifyCors } from '@fastify/cors'
 
 export const corsPlugin = (fastify: any) => {
+  fastify.log.info('Registering CORS plugin...');
+  fastify.log.info('CORS origin: ' + fastify.config.ORIGIN);
   return fastify.register(fastifyCors, {
-    origin: (fastify.config.ORIGIN !== undefined) ? fastify.config.ORIGIN : 'http://localhost:3000',
-    methods: ['GET', 'PUT', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: fastify.config.ORIGIN,
+    methods: ['GET', 'PUT', 'POST', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 };
