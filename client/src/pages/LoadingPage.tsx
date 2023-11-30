8bc8db97b85b375016d11ee2f686d361bcef35b3
dilaouid <dilaouid@student.42.fr>
2023-11-30 21:11:41 +0100

    (+) loadingpage complete

--- client/src/pages/LoadingPage.tsx
+++ client/src/pages/LoadingPage.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
-import { useContext } from "react";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthProvider";
+
+import { IMessage } from "../api/players";
+
 import SteamLoadingIcon from "../components/common/Home/Loading";
 import HeadingTitleComponent from "../components/common/Login/HeadingTitle";
-import { AuthContext } from "../context/AuthProvider";
 import { HelloComponent } from "../components/common/Home/Hello";
+import { ProgressLabelComponent } from "../components/common/Home/ProgressLabel";
 
 const Legend = styled.p`
     margin-bottom: 42px;
@@ -11,6 +17,32 @@ const Legend = styled.p`
 
 export default function LoadingPage () {
     const { auth } = useContext(AuthContext)!;
+    const [ messages, setMessages ] = useState<IMessage[]>([]);
+
+    useEffect(() => {
+        const eventSource = new EventSource(`${BASE_URL}/players/library-checker`, {
+            withCredentials: true
+        });
+      
+        eventSource.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (data.complete) {
+                eventSource.close();
+            }
+            setMessages((messages) => [...messages, data]);
+        };
+      
+        eventSource.onerror = (error) => {
+          console.error('SSE error:', error);
+          eventSource.close();
+    
+        };
+      
+        return () => {
+          eventSource.close();
+        };
+    }, []);
+
     return (
         <section className="py-4 py-xl-5">
             <div className="container">
@@ -20,6 +52,11 @@ export default function LoadingPage () {
                         <SteamLoadingIcon />
                         <HelloComponent username={ auth.user.username } />
                         <Legend className="text-light-emphasis">Nous préparons tout ce qu'il faut pour vous,<br />merci de patienter un instant, ça ne devrait pas être<br />long !&nbsp;</Legend>
+                        {messages.map((msg, index) => (
+                            <div key={index}>
+                                <ProgressLabelComponent message={msg.message} type={msg.type} />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
