e43fac41555a61f1f0d407411d68291a5b3fcd21
dilaouid <dilaouid@student.42.fr>
2023-12-02 20:58:51 +0100

    (+) choose button row action component

---
+++ client/src/components/common/Home/HomePageChooseAction.tsx
@@ -0,0 +1,10 @@
+import CapslockIcon from "../Icons/CapslockIcon";
+import DoorOpenIcon from "../Icons/DoorOpenIcon";
+import ActionButtonComponent from "./ActionButton";
+
+export default function HomePageChooseActionComponent() {
+    return(<div className="row justify-content-center">
+        <ActionButtonComponent text="Rejoindre" target="#join_room" icon={<DoorOpenIcon className="text-primary-emphasis"/>} delay="0s" />
+        <ActionButtonComponent text="Créer" target="#create_room" icon={<CapslockIcon className="text-primary-emphasis" />} delay="1s" />
+    </div>)
+}
