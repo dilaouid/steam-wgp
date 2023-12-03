58f25ca15e2d264f7045902a80c33cb9710e6e9c
dilaouid <dilaouid@student.42.fr>
2023-12-03 21:09:33 +0100

    (+) add modaljoincomponent to homepage

--- client/src/components/common/Home/HomePage.tsx
+++ client/src/components/common/Home/HomePage.tsx
@@ -1,9 +1,11 @@
 import HomePageChooseActionComponent from "./HomePageChooseAction";
 import HomePageHeadingComponent from "./HomePageHeading";
+import ModalJoinComponent from "./ModalJoin";
 
 export default function HomePageComponent() {
     return(
     <div>
+        <ModalJoinComponent />
         <HomePageHeadingComponent />
         <HomePageChooseActionComponent />
     </div>);
