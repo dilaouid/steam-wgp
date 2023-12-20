78f19e93a3cd384916d05ac499a96e29980348fa
dilaouid <dilaouid@student.42.fr>
2023-12-20 14:38:27 +0100

    (~) update fake login

--- server/src/controllers/debug/offline.ts
+++ server/src/controllers/debug/offline.ts
@@ -2,17 +2,16 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { FastifyInstance } from "fastify/types/instance";
 import jwt from "jsonwebtoken"
 
-const userId = 76561198065059816;
-
 export async function loginOfflineMode(request: FastifyRequest, reply: FastifyReply) {
-    const fastify = request.server as FastifyInstance;
-  
-    const jwtToken = jwt.sign({id: String(userId), username: "yedine"}, fastify.config.SECRET_KEY, { expiresIn: '1h' });
-    reply.setCookie("token", jwtToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        path: '/',
-        maxAge: 3600
-    });
-    return reply.redirect(fastify.config.FRONT + '');
+  const fastify = request.server as FastifyInstance;
+
+  // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+  const jwtToken = jwt.sign({id: String(76561198065059816), username: "yedine"}, fastify.config.SECRET_KEY, { expiresIn: '1h' });
+  reply.setCookie("token", jwtToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    maxAge: 3600
+  });
+  return reply.redirect(fastify.config.FRONT + '');
 }
