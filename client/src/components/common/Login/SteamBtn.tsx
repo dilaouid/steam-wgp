c8604a20ac11be5b1848b2ee53306168bc35e520
dilaouid <dilaouid@student.42.fr>
2023-12-20 14:39:06 +0100

    +() login on real online route (no more sahara things)

--- client/src/components/common/Login/SteamBtn.tsx
+++ client/src/components/common/Login/SteamBtn.tsx
@@ -22,7 +22,7 @@ const SteamBtn = styled.img`
 export default function SteamBtnComponent() {
     return (
         // <Link to={ BASE_URL + "/auth/steam" }>
-        <Link to={ BASE_URL + "/debug/login" }>
+        <Link to={ BASE_URL + "/auth/steam" }>
             <SteamBtn className="img-fluid" data-bss-hover-animate="pulse" src="./assets/img/loginsteamlarge.png" alt="openid_steam" loading="lazy" />
         </Link>
     );
