83305c6f7653595d3c9358ffb4d2d4b3ff91c953
dilaouid <dilaouid@student.42.fr>
2023-12-21 10:46:06 +0100

    (~) fix logout

--- client/src/pages/Logout.tsx
+++ client/src/pages/Logout.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react';
 import { logOut } from "../api/auth";
 
-export default async function LogoutPage() {
-    try {
-        await logOut();
-        window.location.href = "/";
-    } catch (err) {
-        console.error("Impossible de déconnecter l'utilisateur: " + err);
-    }
+export default function LogoutPage() {
+    useEffect(() => {
+        const performLogout = async () => {
+            try {
+                await logOut();
+                window.location.href = "/";
+            } catch (err) {
+                console.error("Impossible de déconnecter l'utilisateur: " + err);
+            }
+        };
+
+        performLogout();
+    }, []);
+
     return (
         <div className="container">
-
+            
         </div>
     );
 }
