7a555bb4b70b69b9ee7749fc0a90daec4aec60a6
dilaouid <dilaouid@student.42.fr>
2023-12-23 00:46:23 +0100

    (+) create room button + toaster + redirect to the created room

--- client/src/components/common/Home/ActionButton.tsx
+++ client/src/components/common/Home/ActionButton.tsx
@@ -1,12 +1,16 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import './ActionButton.css';
 import styled from "styled-components";
+import { SpinnerIconComponent } from "../Icons/SpinnerIcon";
+import { toast } from "react-toastify";
+import { APIResponse } from "../../../types/API";
 
 interface ActionButtonComponentProps {
     text: string;
     target: string;
     delay: string;
     icon: ReactNode;
+    onClick?: () => Promise<APIResponse>;
 }
 
 const TextButton = styled.span`
@@ -14,16 +18,49 @@ const TextButton = styled.span`
     font-weight: bolder;
 `;
 
-export default function ActionButtonComponent({ text, target, icon, delay }: ActionButtonComponentProps) {
-    const AnimateDiv = styled.div`
-      --animate-duration: ${delay}s;
-    `;
+const AnimateDiv = styled.div<{ delay: string }>`
+  --animate-duration: ${(props) => props.delay}s;
+`;
 
-    return(<AnimateDiv className="col-auto align-self-center animate__animated animate__fadeInUp">
-        <button className="btn btn-outline-primary bg-black bg-opacity-75 bg-gradient joinOrCreateBtn" type="button" data-bs-target={ target } data-bs-toggle="modal">
-            { icon }
+export default function ActionButtonComponent({ text, target, icon, delay, onClick }: ActionButtonComponentProps) {
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const handleClick = async () => {
+        setIsLoading(true);
+        if (onClick) {
+            try {
+                const waitlist = await onClick();
+                window.location.href = `/waitlist/${waitlist.data.id}`;
+                setIsLoading(false);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            } catch (error: any) {
+                console.error(error);
+                toast.error(error.message, {
+                    position: "bottom-right",
+                    autoClose: 2500,
+                    closeOnClick: true,
+                    theme: "colored",
+                    hideProgressBar: true,
+                });
+                setIsLoading(false);
+            }
+        }
+    };
+
+    return(<AnimateDiv className="col-auto align-self-center animate__animated animate__fadeInUp" delay={delay}>
+        <button 
+            className={`btn bg-black bg-opacity-75 joinOrCreateBtn ${isLoading ? 'disabled btn-outline-secondary' : 'bg-gradient btn-outline-primary'}`}
+            type="button"
+            data-bs-target={onClick ? undefined : target} 
+            data-bs-toggle={onClick ? undefined : "modal"}
+            onClick={onClick ? handleClick : undefined}
+            disabled={isLoading}
+
+        >
+            {isLoading ? <SpinnerIconComponent /> : icon}
             <br />
-            <TextButton>{ text }</TextButton>
+            <TextButton>{ isLoading ? '' : text }</TextButton>
         </button>
     </AnimateDiv>)
 }
--- client/src/components/common/Home/HomePageChooseAction.tsx
+++ client/src/components/common/Home/HomePageChooseAction.tsx
@@ -1,10 +1,27 @@
+import { createRoom } from "../../../api/lobby";
+import { APIResponse } from "../../../types/API";
 import CapslockIcon from "../Icons/CapslockIcon";
 import DoorOpenIcon from "../Icons/DoorOpenIcon";
 import ActionButtonComponent from "./ActionButton";
 
 export default function HomePageChooseActionComponent() {
     return(<div className="row justify-content-center">
-        <ActionButtonComponent text="Rejoindre" target="#join_room" icon={<DoorOpenIcon className="text-primary-emphasis"/>} delay="0.5" />
-        <ActionButtonComponent text="Créer" target="#create_room" icon={<CapslockIcon className="text-primary-emphasis" />} delay="1" />
+        <ActionButtonComponent 
+            text="Rejoindre"
+            target="#join_room"
+            icon={<DoorOpenIcon className="text-primary-emphasis"/>}
+            delay="0.5"
+        />
+
+        <ActionButtonComponent
+            text="Créer"
+            target="#create_room"
+            icon={<CapslockIcon className="text-primary-emphasis" />}
+            delay="1"
+            onClick={async (): Promise<APIResponse> => {
+                return await createRoom();
+            }}
+        
+        />
     </div>)
 }
