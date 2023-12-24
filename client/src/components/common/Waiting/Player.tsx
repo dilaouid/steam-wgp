50045e4d6ce47823bbad9987c5030b2a3ab28c29
dilaouid <dilaouid@student.42.fr>
2023-12-24 12:08:51 +0100

    (+) successfully print all the players in the room

--- client/src/components/common/Waiting/Player.tsx
+++ client/src/components/common/Waiting/Player.tsx
@@ -26,21 +26,21 @@ export default function Player({ player }: { player: PlayerInfo }) {
     const loggedUserIsAdmin = auth.user?.id === room?.admin_id;
     const isLoggedUser = auth.user?.id === player.player_id;
     const loggedUser = room?.players.find(p => p.player_id === auth.user?.id);
+    const gamesInCommon = loggedUser?.games.filter(g => player.games.includes(g)) ?? [];
 
     return (
     <div className="col-auto align-self-center">
-        { player.player_id === room?.admin_id ? <StarIcon /> : <></> }
+        { player.player_id === room?.admin_id ? <StarIcon isLoggedUser={isLoggedUser} /> : <></> }
         { loggedUserIsAdmin && player.player_id !== room?.admin_id ? <KickButton playerId={player.player_id} /> : <></> }
-        <OverlayTrigger
-            placement="top"
-            overlay={<Tooltip id={`Tooltip for Player #${player.player_id}`}>{ player.username }</Tooltip>}
-        >
-            <img className="img-fluid profil_picture_room" src={`https://avatars.akamai.steamstatic.com/${player?.avatar_hash}_full.jpg`} loading="lazy" />
+        <OverlayTrigger placement="top" overlay={<Tooltip id={`Tooltip for Player #${player.player_id}`}>{ player.username }</Tooltip>}>
+            <img className={`img-fluid profil_picture_room ${!isLoggedUser ? 'other_player_wroom' : ''}`} src={`https://avatars.akamai.steamstatic.com/${player?.avatar_hash}_full.jpg`} loading="lazy" />
         </OverlayTrigger>
         { !isLoggedUser ?
-        <GamesInCommon className="lead text-center text-primary text-bold" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" title="9 games in common">
-            { loggedUser?.games.filter(g => player.games.includes(g)).length }
-            <ControllerIcon />
-        </GamesInCommon> : <></>}
+        <OverlayTrigger placement="bottom" overlay={<Tooltip id={`Tooltip for Games in Commun with #${player.player_id}`}>{ gamesInCommon.length } jeu{ gamesInCommon.length > 1 ? 'x' : ''} en commun</Tooltip>}>
+            <GamesInCommon className="lead text-center text-primary text-bold">
+                { gamesInCommon.length }
+                <ControllerIcon />
+            </GamesInCommon>
+        </OverlayTrigger> : <></>}
     </div>);
 }
