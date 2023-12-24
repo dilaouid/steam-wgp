4c52e5fbbb980717a03bacdfa62cb0755db5b067
dilaouid <dilaouid@student.42.fr>
2023-12-24 21:05:21 +0100

    (~) disable start button if requirements not met

--- client/src/components/common/Waiting/RoomActionButtons.tsx
+++ client/src/components/common/Waiting/RoomActionButtons.tsx
@@ -26,11 +26,13 @@ export const RoomActionButtons: React.FC = () => {
     const commonGames = calculateCommonGames();
     const playersInRoom = room.players.length;
 
+    const disabled = !commonGames || playersInRoom <= 1;
+
     return (
     <Row className="justify-content-center">
         { isAdmin ?
         <Col xs="auto">
-            <Button variant="outline-primary" size="lg" disabled={!commonGames || playersInRoom <= 1}>Démarrer</Button>
+            <Button variant={disabled ? "outline-light" : "outline-primary"} size="lg" disabled={disabled}>Démarrer</Button>
         </Col> : <></> }
         <Col xs="auto">
             <Button variant="outline-danger" size="lg">Quitter</Button>
