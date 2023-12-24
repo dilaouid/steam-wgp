1b671b5436d718ea247852c1a9b95eb87eb41cc4
dilaouid <dilaouid@student.42.fr>
2023-12-24 10:35:05 +0100

    (+) create KickIcon and ControllerIcon

---
+++ client/src/components/common/Icons/KickIcon.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Icon = styled.svg`
+    font-size: 29px;
+`;
+
+export const KickIconComponent: React.FC = () => {
+    return <Icon xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-person-fill-x text-white">
+        <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"></path>
+        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708Z"></path>
+    </Icon>;
+};
