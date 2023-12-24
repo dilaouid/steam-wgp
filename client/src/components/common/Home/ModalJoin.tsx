dc26399ed77c0eb49885c1b891c5d29d82d850fb
dilaouid <dilaouid@student.42.fr>
2023-12-24 21:04:55 +0100

    (+) successfully join / leave room from modal + remove backdrop after navigate()

--- client/src/components/common/Home/ModalJoin.tsx
+++ client/src/components/common/Home/ModalJoin.tsx
@@ -1,12 +1,10 @@
 import { useState, useContext } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components"
 
 import { Auth } from "../../../context";
 import { joinOrLeaveRoom } from "../../../api/lobby";
-
-const Alert = styled.div`
-    margin-top: 16px;
-`;
 
 const ModalBody = styled.div`
     background: url('assets/img/dots.png');
@@ -21,24 +19,59 @@ const Modal = styled.div`
 export default function ModalJoinComponent() {
     const { setAuth } = useContext(Auth.Context)!;
     const [ uuid, setUuid ] = useState('');
-    const [ alert, setAlert ] = useState({ type: '', message: '', display: false });
+    const [ loading, setLoading ] = useState(false);
+
+    const navigate = useNavigate();
 
     const isValidUuid = (uuid: string) => {
         return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(uuid);
     };
 
     const handleSubmit = async () => {
+        setLoading(true)
         if (!isValidUuid(uuid)) {
-            setAlert({ type: 'danger', message: 'UUID non valide', display: true });
+            toast.error('UUID non valide!', {
+                position: "bottom-right",
+                autoClose: 2500,
+                closeOnClick: true,
+                theme: "colored",
+                hideProgressBar: false,
+            });
+            setLoading(false);
             return;
         }
         
         try {
-            await joinOrLeaveRoom(uuid, setAuth);
-            window.location.href = '/waitlist/' + uuid;
-        } catch (error) {
-            console.log(error);
-            setAlert({ type: 'danger', message: 'Erreur lors de la soumission', display: true });
+            const joinOrLeave = await joinOrLeaveRoom(uuid, setAuth);
+            if (joinOrLeave.data.action === 'leave') {
+                toast.success('Vous avez quitté la room!', {
+                    position: "bottom-right",
+                    autoClose: 2500,
+                    closeOnClick: true,
+                    theme: "colored",
+                    hideProgressBar: false,
+                });
+            } else {
+                setTimeout(() => {
+                    const modals = document.querySelectorAll('.modal-backdrop');
+                    modals.forEach((modal) => {
+                        modal.remove();
+                    });
+                    navigate('/waitlist/' + uuid);
+                }, 500);
+
+            }
+            setLoading(false);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+            toast.error(error.message, {
+                position: "bottom-right",
+                autoClose: 2500,
+                closeOnClick: true,
+                theme: "colored",
+                hideProgressBar: false,
+            });
+            setLoading(false);
         }
     };
 
@@ -58,17 +91,12 @@ export default function ModalJoinComponent() {
                             required
                         />
                         <button 
-                            className={`btn btn-primary ${!isValidUuid(uuid) ? "disabled" : ""}`}
+                            className={`btn btn-primary ${!isValidUuid(uuid) || loading ? "disabled" : ""}`}
                             type="button"
                             onClick={handleSubmit}
-                            disabled={!isValidUuid(uuid)}
-                        >Rejoindre</button>
+                            disabled={!isValidUuid(uuid) || loading}
+                        >{ loading ? "" : "Rejoindre" }</button>
                     </div>
-                    { alert.display &&
-                        <Alert className={"alert alert-" + alert.type} role="alert">
-                            <span>{ alert.message }</span>
-                        </Alert>
-                    }
                 </ModalBody>
             </div>
         </div>
