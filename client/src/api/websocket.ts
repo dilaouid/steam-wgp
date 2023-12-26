24088b4ca5a586bef588ee80261ca79ba4d71015
dilaouid <dilaouid@student.42.fr>
2023-12-26 12:54:40 +0100

    (+) websocket functions client side

---
+++ client/src/api/websocket.ts
@@ -0,0 +1,40 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const connectWebSocket = (waitlistId: string, token: string): WebSocket => {
+    const wsUrl = `${BASE_URL}/ws/${waitlistId}`;
+    const socket = new WebSocket(wsUrl, [token]);
+
+    socket.onopen = () => {
+        console.log('WebSocket connection established');
+    };
+
+    socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        console.log('Data received from server', data);
+    };
+
+    socket.onclose = () => {
+        console.log('WebSocket connection closed');
+    };
+
+    socket.onerror = (error) => {
+        console.error('WebSocket error', error);
+    };
+
+    return socket;
+};
+
+export const swipeCard = (socket: WebSocket, gameId: string) => {
+    const message = JSON.stringify({ action: 'swipe', payload: { gameId } });
+    socket.send(message);
+};
+
+export const startWaitlist = (socket: WebSocket) => {
+    const message = JSON.stringify({ action: 'start' });
+    socket.send(message);
+};
+
+export const leaveWaitlist = (socket: WebSocket) => {
+    const message = JSON.stringify({ action: 'leave' });
+    socket.send(message);
+};
