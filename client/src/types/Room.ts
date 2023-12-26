ea5676fbec869934260d84311df840b796cdc158
dilaouid <dilaouid@student.42.fr>
2023-12-26 12:47:28 +0100

    (+) improve roominfo interface

--- client/src/types/Room.ts
+++ client/src/types/Room.ts
@@ -4,6 +4,8 @@ export interface RoomInfo {
     admin_id: string;
     id: string;
     started: boolean;
+    ended: boolean;
+    swipedGames: Record<number, string[]>;
     commonGames: number[];
     players: PlayerInfo[];
     updated_at: Date;
