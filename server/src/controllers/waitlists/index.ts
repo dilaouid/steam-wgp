bde8faced22b42054ff177d403bb0e3a86ec46fe
dilaouid <dilaouid@student.42.fr>
2023-12-27 15:03:17 +0100

    (+) route and controller to kick a player from a room

--- server/src/controllers/waitlists/index.ts
+++ server/src/controllers/waitlists/index.ts
@@ -1,9 +1,11 @@
 import { createWaitlistOpts } from "./createWaitlistRoom";
 import { getWaitlistWithPlayersOpts } from "./getWaitlistRoom";
 import { joinOrLeaveWaitlistOpts } from "./joinWaitlistRoom";
+import { kickFromWaitlistOpts } from "./kickFromWaitlistRoom";
 
 export {
   createWaitlistOpts,
   joinOrLeaveWaitlistOpts,
-  getWaitlistWithPlayersOpts
+  getWaitlistWithPlayersOpts,
+  kickFromWaitlistOpts
 }
--- server/src/router/waitlistRouter.ts
+++ server/src/router/waitlistRouter.ts
@@ -4,13 +4,15 @@ import { FastifyInstance } from 'fastify';
 import {
   createWaitlistOpts as createWaitlist,
   getWaitlistWithPlayersOpts as getWaitlist,
-  joinOrLeaveWaitlistOpts as joinOrLeaveWaitlist } from '../controllers/waitlists';
+  joinOrLeaveWaitlistOpts as joinOrLeaveWaitlist,
+  kickFromWaitlistOpts as kickFromWaitlist } from '../controllers/waitlists';
 
 export default async function waitlistRouter(fastify: FastifyInstance) {
   fastify.register(function (app: FastifyInstance, _, done) {
-    app.route(createWaitlist),        // :POST  /waitlist
-    app.route(getWaitlist);           // :GET   /waitlist/:id
-    app.route(joinOrLeaveWaitlist);   // :PATCH /waitlist/:id
+    app.route(createWaitlist),        // :POST    /waitlist
+    app.route(getWaitlist);           // :GET     /waitlist/:id
+    app.route(joinOrLeaveWaitlist);   // :PATCH   /waitlist/:id
+    app.route(kickFromWaitlist);      // :DELETE  /waitlist/:id/kick/:playerId
     done();
   });
 }
