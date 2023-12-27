0634ba71b8851548906ebacc2c6e43ac893c9e43
dilaouid <dilaouid@student.42.fr>
2023-12-27 23:59:44 +0100

    (-) remove useless print of cors

--- server/src/plugins/cors.plugin.ts
+++ server/src/plugins/cors.plugin.ts
@@ -1,8 +1,6 @@
 import { fastifyCors } from '@fastify/cors'
 
 export const corsPlugin = (fastify: any) => {
-  fastify.log.info('Registering CORS plugin...');
-  fastify.log.info('CORS origin: ' + fastify.config.ORIGIN);
   return fastify.register(fastifyCors, {
     origin: fastify.config.ORIGIN,
     methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
