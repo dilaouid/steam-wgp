e286b0317b572def166fb9e43292dfb31671050a
dilaouid <dilaouid@student.42.fr>
2023-12-29 13:21:56 +0100

    (+) create HeartIcon component

---
+++ client/src/components/common/Icons/HeartIcon.tsx
@@ -0,0 +1,5 @@
+export const HeartIcon: React.FC = () => {
+    return <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-heart-fill">
+    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
+</svg>;
+};
