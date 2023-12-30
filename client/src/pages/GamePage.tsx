d27268496187360c5deb423db9098c8d76e7ad66
dilaouid <dilaouid@student.42.fr>
2023-12-30 15:54:21 +0100

    (+) animate result

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { Room, WebSocket } from "../context";
 import { HeartIcon } from "../components/common/Icons/HeartIcon";
@@ -8,9 +8,19 @@ import { Spinner } from "react-bootstrap";
 export default function GamePage () {
     const { room, setRoom } = useContext(Room.Context)!;
     const [ index, setIndex ] = useState<number>(0);
+    const [animate, setAnimate] = useState(false);
     const socket = useContext(WebSocket.Context)!;
 
+    useEffect(() => {
+        if (room?.winner) {
+            setTimeout(() => {
+                setAnimate(true);
+            }, 1000);
+        }
+    }, [room?.winner])
+
     if (!room || !socket) return <></>;
+
 
     const swipe = (like: boolean, game: number) => {
         if (!socket.socket || !room || !game) return;
@@ -52,42 +62,47 @@ export default function GamePage () {
     };
 
     return (
-        <div className="game-page-container">
-            <div className="card-stack" style={{
-                        transform: `translateY(${index * -700}px)`,
-                    }}>
-                {room.commonGames.map((game:number) => (
-                    <div key={game} >
-                        <div
-                            className={`card ${index === getGameIndex(game) ? 'active' : ''}`}
-                            style={{
-                                backgroundImage: `url(https://steamcdn-a.akamaihd.net/steam/apps/${game}/library_600x900.jpg)`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                width: '100%',
-                                height: '100%'
-                            }}
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className="action-buttons">
-                <button className="btn btn-danger btn-lg" onClick={() => swipe(true, room.commonGames[index])}>
-                    <HeartIcon /> J'aime !
-                </button>
-                <button className="btn btn-outline-warning btn-lg" onClick={() => swipe(false, room.commonGames[index])}>
-                    Pas intéressé
-                </button>
-                <p className="text-light">Aimez ou ignorez les jeux affichés. Le premier jeu qui aura été aimé par tout les joueurs de la room sera affiché, et vous saurez comment gaspiller les prochaines heures de votre précieuse vie.</p>
-                <p className="text-info-emphasis"><Spinner color={"info"} size="sm" /> La room sélectionnera un jeu aléatoire 5 minutes après sa création si aucun choix n'a pas été fait ...</p>
-            </div>
-            <div className="game-page-background-container">
-                { room.swipedGames?.length > 0 ? <h5 className="text-warning-emphasis">Cliquez sur un jeu pour le retirer de vos J'aimes</h5> : <> </> }
-                {room.swipedGames?.map((game: number) => (
-                    <img key={game} src={`https://steamcdn-a.akamaihd.net/steam/apps/${game}/header.jpg?t=1628007606`} alt="Swipped game" className="game-page-background" onClick={() => unlike(game)} />
-                ))}
-            </div>
+        <div className="container">
+        { !room.ended ?<div className="game-page-container">
+        <div className="card-stack" style={{
+                    transform: `translateY(${index * -700}px)`,
+                }}>
+            {room.commonGames.map((game:number) => (
+                <div key={game} >
+                    <div
+                        className={`card ${index === getGameIndex(game) ? 'active' : ''}`}
+                        style={{
+                            backgroundImage: `url(https://steamcdn-a.akamaihd.net/steam/apps/${game}/library_600x900.jpg)`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            width: '100%',
+                            height: '100%'
+                        }}
+                    />
+                </div>
+            ))}
         </div>
+        <div className="action-buttons">
+            <button className="btn btn-danger btn-lg" onClick={() => swipe(true, room.commonGames[index])}>
+                <HeartIcon /> J'aime !
+            </button>
+            <button className="btn btn-outline-warning btn-lg" onClick={() => swipe(false, room.commonGames[index])}>
+                Pas intéressé
+            </button>
+            <p className="text-light">Aimez ou ignorez les jeux affichés. Le premier jeu qui aura été aimé par tout les joueurs de la room sera affiché, et vous saurez comment gaspiller les prochaines heures de votre précieuse vie.</p>
+            <p className="text-info-emphasis"><Spinner color={"info"} size="sm" /> La room sélectionnera un jeu aléatoire 5 minutes après sa création si aucun choix n'a pas été fait ...</p>
+        </div>
+        <div className="game-page-background-container">
+            { room.swipedGames?.length > 0 ? <h5 className="text-warning-emphasis">Cliquez sur un jeu pour le retirer de vos J'aimes</h5> : <> </> }
+            {room.swipedGames?.map((game: number) => (
+                <img key={game} src={`https://steamcdn-a.akamaihd.net/steam/apps/${game}/header.jpg?t=1628007606`} alt="Swipped game" className="game-page-background" onClick={() => unlike(game)} />
+            ))}
+        </div>
+    </div> : <div className="victory align-self-center d-flex justify-content-center">
+        <img src={`https://steamcdn-a.akamaihd.net/steam/apps/${room.winner}/library_600x900.jpg`} className={animate ? 'animate' : ''}  />
+        </div> }
+    </div>
+        
     );
 }
