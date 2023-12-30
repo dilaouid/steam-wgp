745f5bb3400c8319dfbb84df1a9d3164ffabbd20
dilaouid <dilaouid@student.42.fr>
2023-12-30 12:30:12 +0100

    (+) unlike a game (client side)

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react";
 import { Room, WebSocket } from "../context";
 import { HeartIcon } from "../components/common/Icons/HeartIcon";
 import { swipeCard } from "../api/websocket";
+import { Spinner } from "react-bootstrap";
 
 export default function GamePage () {
     const { room, setRoom } = useContext(Room.Context)!;
@@ -14,7 +15,6 @@ export default function GamePage () {
     const swipe = (like: boolean, game: number) => {
         if (!socket.socket || !room || !game) return;
         if (like) {
-            swipeCard(socket.socket, game);
             setRoom(prevRoom => {
                 if (!prevRoom) return null;
                 
@@ -24,12 +24,27 @@ export default function GamePage () {
                     swipedGames: prevRoom.swipedGames ? [...prevRoom.swipedGames, game] : [game]
                 };
             });
+            swipeCard(socket.socket, game);
             if (index === room.commonGames.length - 1) {
                 setIndex(0);
             }
         } else {
             setIndex(prevIndex => (prevIndex + 1) % room.commonGames.length);
         }
+    }
+
+    const unlike = (game: number) => {
+        if (!socket.socket || !room || !game) return;
+        setRoom(prevRoom => {
+            if (!prevRoom) return null;
+            
+            return {
+                ...prevRoom,
+                // put the game back in the commonGames array in the current index
+                commonGames: [...prevRoom.commonGames.slice(0, index), game, ...prevRoom.commonGames.slice(index)],
+                swipedGames: prevRoom.swipedGames?.filter(g => g !== game)
+            };
+        });
     }
 
     const getGameIndex = (game: number) => {
@@ -65,6 +80,13 @@ export default function GamePage () {
                     Pas intéressé
                 </button>
                 <p className="text-light">Aimez ou ignorez les jeux affichés. Le premier jeu qui aura été aimé par tout les joueurs de la room sera affiché, et vous saurez comment gaspiller les prochaines heures de votre précieuse vie.</p>
+                <p className="text-info-emphasis"><Spinner color={"info"} size="sm" /> La room sélectionnera un jeu aléatoire 5 minutes après sa création si aucun choix n'a pas été fait ...</p>
+            </div>
+            <div className="game-page-background-container">
+                { room.swipedGames?.length > 0 ? <h5 className="text-warning-emphasis">Cliquez sur un jeu pour le retirer de vos J'aimes</h5> : <> </> }
+                {room.swipedGames?.map((game: number) => (
+                    <img key={game} src={`https://steamcdn-a.akamaihd.net/steam/apps/${game}/header.jpg?t=1628007606`} alt="Swipped game" className="game-page-background" onClick={() => unlike(game)} />
+                ))}
             </div>
         </div>
     );
