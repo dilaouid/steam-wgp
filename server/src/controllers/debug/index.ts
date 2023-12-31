d0d38107554b3748641b050127e239aafac145c8
dilaouid <dilaouid@student.42.fr>
2023-12-31 15:40:40 +0100

    (-) delete shitty controller

--- server/src/controllers/debug/index.ts
+++ server/src/controllers/debug/index.ts
@@ -1,11 +1,9 @@
 import { truncateAll } from "./truncate";
 import { getAllEnvVariables } from "./env";
-import { TestRouter } from "./test";
 import { loginOfflineMode } from "./offline";
 
 export {
   truncateAll,
   getAllEnvVariables,
-  TestRouter,
   loginOfflineMode
 }
--- server/src/router/debugRouter.ts
+++ server/src/router/debugRouter.ts
@@ -1,11 +1,10 @@
 import { FastifyInstance } from 'fastify';
-import { TestRouter, getAllEnvVariables, truncateAll, loginOfflineMode } from '../controllers/debug';
+import { getAllEnvVariables, truncateAll, loginOfflineMode } from '../controllers/debug';
 
 export default async function debugRouter(fastify: FastifyInstance) {
   fastify.register(async function (fastify) {
     fastify.get('/env', getAllEnvVariables);
     fastify.get('/truncate', truncateAll);
-    fastify.get('/test', TestRouter);
     fastify.get('/login', loginOfflineMode);
   });
 }
