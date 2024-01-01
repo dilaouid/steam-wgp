5c9e7ba378c34d7e6eb38b4eafcbc5f9e7c3fe4f
dilaouid <dilaouid@student.42.fr>
2024-01-01 22:34:04 +0100

    (+) second try for react-router in gh-pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+
 import App from './App.tsx'
 import './index.css'
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
