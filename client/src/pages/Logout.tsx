781fcf146e20843b3adbab4588a928fb62a75f21
dilaouid <dilaouid@student.42.fr>
2024-01-02 15:22:46 +0100

    (+) navigate to homepage after logout

--- client/src/pages/Logout.tsx
+++ client/src/pages/Logout.tsx
@@ -1,19 +1,21 @@
 import { useEffect } from 'react';
 import { logOut } from "../api/auth";
+import { useNavigate } from 'react-router-dom';
 
 export default function LogoutPage() {
+    const navigate = useNavigate();
     useEffect(() => {
         const performLogout = async () => {
             try {
                 await logOut();
-                window.location.href = "/";
+                navigate('/');
             } catch (err) {
                 console.error("Impossible de déconnecter l'utilisateur: " + err);
             }
         };
 
         performLogout();
-    }, []);
+    }, [ navigate ]);
 
     return (
         <div className="container">
