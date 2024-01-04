9334509a708edcd0f4dc20eb35bb1cb1c1788380
dilaouid <dilaouid@student.42.fr>
2024-01-04 23:07:03 +0100

    (+) when in login page & authenticated redirect to home

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import HeadingContainerComponent from '../components/common/Login/HeadingContainer';
+import { useContext } from 'react';
+import { Auth } from '../context';
 
 export default function LoginPage() {
+    const { auth } = useContext(Auth.Context)!;
+    const navigate = useNavigate();
+    
+    if (auth.isAuthenticated) {
+        window.history.replaceState(null, '', '/');
+        window.history.pushState(null, '', '/');
+        window.history.go(0);
+        navigate('/');
+    }
     return (
         <div className="container">
             <HeadingContainerComponent />
