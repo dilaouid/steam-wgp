65919028eb10bc1ff063044b3ac09b511cf7e898
dilaouid <dilaouid@student.42.fr>
2024-01-04 11:52:21 +0100

    (+) fix jwt expired error in the get steam library

--- server/src/controllers/players/getSteamLibrary.ts
+++ server/src/controllers/players/getSteamLibrary.ts
@@ -1,7 +1,6 @@
 import { EventMessage, FastifyInstance, FastifyReply, FastifyRequest, HTTPMethods } from "fastify";
 import { Games, Libraries } from "../../models";
 import { eq, inArray } from 'drizzle-orm';
-import { isAuthenticated } from "../../auth/mw";
 import { Player } from "../../models/Players";
 import { APIResponse } from "../../utils/response";
 import jwt from 'jsonwebtoken';
@@ -73,90 +72,100 @@ async function getSteamLibrary(request: FastifyRequest<{ Querystring: IQS }>, re
 
   const token = request.query.token as string;
 
-  const secretKey = fastify.config.SECRET_KEY;
-  const decoded = jwt.verify(token, secretKey);
+  try {
+    const secretKey = fastify.config.SECRET_KEY;
+    const decoded = jwt.verify(token, secretKey);
 
-  if (!decoded) {
+    if (!decoded) {
+      return APIResponse(reply, null, 'Accès interdit', 401);
+    }
+
+    const { id } = decoded as Player;
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+      'Access-Control-Allow-Origin': fastify.config.ORIGIN,
+      'Access-Control-Allow-Credentials': 'true'
+    };
+
+    reply.raw.writeHead(200, headers);
+
+    if (!id)
+      return APIResponse(reply, null, 'Vous devez être connecté pour créer une room', 401);
+    reply.sse((async function* (): EventMessage | AsyncIterable<EventMessage> {
+      try {
+        yield { data: JSON.stringify({ message: 'Chargement de ta bibliothèque Steam ...', type: 'info', complete: false }) }
+        const library = await fastify.db.select({ game_id: Libraries.model.game_id }).from(Libraries.model).where(eq(Libraries.model.player_id, id));
+        const playerLibraryIds = new Set(library.map((game: ILibrary) => game.game_id));
+
+        fastify.log.info(`Fetching Steam library for player ${id}...`);
+        const steamLibraryRequest = await fetch(`${fastify.config.STEAM_GetOwnedGames}/?key=${fastify.config.STEAM_API_KEY}&steamid=${id}&include_appinfo=true&include_played_free_games=true&format=json`);
+        if (steamLibraryRequest == null || steamLibraryRequest.status !== 200) {
+          fastify.log.warn(`Steam API is not responding...`);
+          yield { data: JSON.stringify({ message: 'La bibliothèque Steam n\'est pas accessible pour le moment', type: 'danger', complete: true }) };
+          return;
+        }
+        const steamLibrary = await steamLibraryRequest.json() as ISteamResponse;
+
+        if (steamLibrary.response && !steamLibrary.response?.games) {
+          yield { data: JSON.stringify({ message: 'Votre bibliothèque Steam n\'est pas accessible pour le moment. Votre compte est peut-être privé, ou alors vous n\'avez pas de jeu.', type: 'danger', complete: false }) };
+          return;
+        }
+
+        fastify.log.info('Steam library fetched successfully !');
+        if (!steamLibrary.response) {
+          fastify.log.warn(`Steam API is not responding...`);
+          yield { data: JSON.stringify({ message: 'La bibliothèque Steam n\'est pas accessible pour le moment', type: 'danger', complete: true }) };
+          return;
+        }
+
+        yield { data: JSON.stringify({ message: 'Ajout des jeux à la bibliothèque ...', type: 'info', complete: false }) };
+        const steamAppIds = steamLibrary.response.games.map((game: ILibrary) => game.appid);
+        const gamesToAddToLibrary = steamAppIds.filter((gameId: number) => !playerLibraryIds.has(gameId));
+
+        // get the app ids that are not in the database yet
+        fastify.log.info(`Fetching games details from Steam API...`);
+        const appIdsNotInDB = await getAppIdsNotInDB(fastify, steamAppIds);
+        fastify.log.info(`\n--- ${appIdsNotInDB.length} games are not in the database yet ---\n`);
+
+        // insert the games in the Games table if they are not already in the database
+        fastify.log.info(`Inserting games into the database...`);
+        const gamesToAdd = await Promise.all(appIdsNotInDB.map(async appId => await fetchGameDetails(fastify, appId).catch(err => {
+          fastify.log.error(err);
+          return { game_id: appId, is_selectable: false }
+        })))
+        const filteredGamesToAdd = gamesToAdd.filter(game => game !== null);
+        fastify.log.info(`\n--- We are about to insert ${filteredGamesToAdd.length} games into the database...\n`);
+        await insertGames(fastify, filteredGamesToAdd);
+
+        // insert the games in the Libraries table if they are not already in the database
+        fastify.log.info(`Inserting games into the library...`);
+        yield { data: JSON.stringify({ message: 'Ajout des jeux à ta collection ...', type: 'info', complete: false }) };
+
+        await insertGamesIntoLibrary(fastify, BigInt(id), gamesToAddToLibrary);
+
+        // if added games in db is less than gamesToAddToLibrary, then warn the user
+        if (gamesToAddToLibrary.length > 0 && gamesToAddToLibrary.length < gamesToAdd.length) {
+          if (gamesToAdd.length <= 1) yield { data: JSON.stringify({ message: `${gamesToAdd.length} jeu n'a pas pu être ajouté à la bibliothèque !`, type: 'danger' }) };
+          else yield { data: JSON.stringify({ message: `${gamesToAdd.length} jeux n'ont pas pu être ajoutés à la bibliothèque !`, type: 'danger', complete: false }) };
+
+          fastify.log.warn(`Only ${gamesToAdd.length} out of ${gamesToAddToLibrary.length} games were added to the database`);
+        }
+        yield { data: JSON.stringify({ message: 'Fin du processus de mise à jour de la bibliothèque, vous allez maintenant être redirigé', type: 'success', complete: true }) }
+      } catch (err) {
+        fastify.log.error(err);
+        yield { data: JSON.stringify({ message: 'Une erreur est survenue lors de la mise à jour de ta bibliothèque', type: 'danger', complete: true }) };
+      }
+    })());
+  } catch (error: any) {
+    // if the error is due to an expired token
+    if (error.type === 'TokenExpiredError') {
+      reply.clearCookie('token');
+      return APIResponse(reply, null, 'Votre session a expiré, veuillez vous reconnecter', 401);
+    }
+
     return APIResponse(reply, null, 'Accès interdit', 401);
   }
 
-  const { id } = decoded as Player;
-
-  const headers = {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Access-Control-Allow-Origin': fastify.config.ORIGIN,
-    'Access-Control-Allow-Credentials': 'true'
-  };
-
-  reply.raw.writeHead(200, headers);
-
-  if (!id)
-    return APIResponse(reply, null, 'Vous devez être connecté pour créer une room', 401);
-  reply.sse((async function* (): EventMessage | AsyncIterable<EventMessage> {
-    try {
-      yield { data: JSON.stringify({ message: 'Chargement de ta bibliothèque Steam ...', type: 'info', complete: false }) }
-      const library = await fastify.db.select({ game_id: Libraries.model.game_id }).from(Libraries.model).where(eq(Libraries.model.player_id, id));
-      const playerLibraryIds = new Set(library.map((game: ILibrary) => game.game_id));
-
-      fastify.log.info(`Fetching Steam library for player ${id}...`);
-      const steamLibraryRequest = await fetch(`${fastify.config.STEAM_GetOwnedGames}/?key=${fastify.config.STEAM_API_KEY}&steamid=${id}&include_appinfo=true&include_played_free_games=true&format=json`);
-      if (steamLibraryRequest == null || steamLibraryRequest.status !== 200) {
-        fastify.log.warn(`Steam API is not responding...`);
-        yield { data: JSON.stringify({ message: 'La bibliothèque Steam n\'est pas accessible pour le moment', type: 'danger', complete: true }) };
-        return;
-      }
-      const steamLibrary = await steamLibraryRequest.json() as ISteamResponse;
-
-      if (steamLibrary.response && !steamLibrary.response?.games) {
-        yield { data: JSON.stringify({ message: 'Votre bibliothèque Steam n\'est pas accessible pour le moment. Votre compte est peut-être privé, ou alors vous n\'avez pas de jeu.', type: 'danger', complete: false }) };
-        return;
-      }
-
-      fastify.log.info('Steam library fetched successfully !');
-      if (!steamLibrary.response) {
-        fastify.log.warn(`Steam API is not responding...`);
-        yield { data: JSON.stringify({ message: 'La bibliothèque Steam n\'est pas accessible pour le moment', type: 'danger', complete: true }) };
-        return;
-      }
-
-      yield { data: JSON.stringify({ message: 'Ajout des jeux à la bibliothèque ...', type: 'info', complete: false }) };
-      const steamAppIds = steamLibrary.response.games.map((game: ILibrary) => game.appid);
-      const gamesToAddToLibrary = steamAppIds.filter((gameId: number) => !playerLibraryIds.has(gameId));
-
-      // get the app ids that are not in the database yet
-      fastify.log.info(`Fetching games details from Steam API...`);
-      const appIdsNotInDB = await getAppIdsNotInDB(fastify, steamAppIds);
-      fastify.log.info(`\n--- ${appIdsNotInDB.length} games are not in the database yet ---\n`);
-
-      // insert the games in the Games table if they are not already in the database
-      fastify.log.info(`Inserting games into the database...`);
-      const gamesToAdd = await Promise.all(appIdsNotInDB.map(async appId => await fetchGameDetails(fastify, appId).catch(err => {
-        fastify.log.error(err);
-        return { game_id: appId, is_selectable: false }
-      })))
-      const filteredGamesToAdd = gamesToAdd.filter(game => game !== null);
-      fastify.log.info(`\n--- We are about to insert ${filteredGamesToAdd.length} games into the database...\n`);
-      await insertGames(fastify, filteredGamesToAdd);
-
-      // insert the games in the Libraries table if they are not already in the database
-      fastify.log.info(`Inserting games into the library...`);
-      yield { data: JSON.stringify({ message: 'Ajout des jeux à ta collection ...', type: 'info', complete: false }) };
-
-      await insertGamesIntoLibrary(fastify, BigInt(id), gamesToAddToLibrary);
-
-      // if added games in db is less than gamesToAddToLibrary, then warn the user
-      if (gamesToAddToLibrary.length > 0 && gamesToAddToLibrary.length < gamesToAdd.length) {
-        if (gamesToAdd.length <= 1) yield { data: JSON.stringify({ message: `${gamesToAdd.length} jeu n'a pas pu être ajouté à la bibliothèque !`, type: 'danger' }) };
-        else yield { data: JSON.stringify({ message: `${gamesToAdd.length} jeux n'ont pas pu être ajoutés à la bibliothèque !`, type: 'danger', complete: false }) };
-
-        fastify.log.warn(`Only ${gamesToAdd.length} out of ${gamesToAddToLibrary.length} games were added to the database`);
-      }
-      yield { data: JSON.stringify({ message: 'Fin du processus de mise à jour de la bibliothèque, vous allez maintenant être redirigé', type: 'success', complete: true }) }
-    } catch (err) {
-      fastify.log.error(err);
-      yield { data: JSON.stringify({ message: 'Une erreur est survenue lors de la mise à jour de ta bibliothèque', type: 'danger', complete: true }) };
-    }
-  })());
 }
