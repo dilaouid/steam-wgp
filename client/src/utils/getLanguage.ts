e72d0ca3282440d35c057620672d09b5ded248b7
dilaouid <dilaouid@student.42.fr>
2024-01-11 00:11:51 +0100

    (~) update getting language from localstorage

--- client/src/utils/getLanguage.ts
+++ client/src/utils/getLanguage.ts
@@ -1,17 +1,19 @@
 const availableLanguages = ['fr', 'en'];
 
 export const getBrowserLanguage = () => {
-    const language = localStorage.getItem('language');
+    const language = localStorage.getItem('i18nextLng');
     if (language && availableLanguages.includes(language)) {
         return language;
     } else {
-        localStorage.removeItem('language');
+        localStorage.removeItem('i18nextLng');
     }
 
     const browserLanguage = navigator.language.split('-')[0];
     if (availableLanguages.includes(browserLanguage)) {
+        localStorage.setItem('i18nextLng', browserLanguage);
         return browserLanguage;
     }
 
+    localStorage.setItem('i18nextLng', 'en');
     return 'en';
 };
