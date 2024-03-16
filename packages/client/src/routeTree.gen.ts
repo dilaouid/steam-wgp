af16c31d057af75fa96367674a8139103ae93043
dilaouid <dilaouid@student.42.fr>
2024-03-16 12:18:37 +0100

    (+) init login page

--- packages/client/src/routeTree.gen.ts
+++ packages/client/src/routeTree.gen.ts
@@ -16,9 +16,15 @@ import { Route as rootRoute } from './routes/__root'
 
 // Create Virtual Routes
 
+const LoginLazyImport = createFileRoute('/login')()
 const IndexLazyImport = createFileRoute('/')()
 
 // Create/Update Routes
+
+const LoginLazyRoute = LoginLazyImport.update({
+  path: '/login',
+  getParentRoute: () => rootRoute,
+} as any).lazy(() => import('./routes/login.lazy').then((d) => d.Route))
 
 const IndexLazyRoute = IndexLazyImport.update({
   path: '/',
@@ -33,11 +39,15 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexLazyImport
       parentRoute: typeof rootRoute
     }
+    '/login': {
+      preLoaderRoute: typeof LoginLazyImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([IndexLazyRoute])
+export const routeTree = rootRoute.addChildren([IndexLazyRoute, LoginLazyRoute])
 
 /* prettier-ignore-end */
---
+++ packages/client/src/routes/login.lazy.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { createLazyFileRoute } from '@tanstack/react-router'
+import { Loginpage } from '../components/templates/Loginpage';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export const Route = createLazyFileRoute("/login")({
+  component: Login,
+})
+
+function Login() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  return <Loginpage />
+}
