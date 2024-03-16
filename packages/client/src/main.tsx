5eaa1de8f6c313b52c0d2bc53d138362746adaa6
dilaouid <dilaouid@student.42.fr>
2024-03-16 09:16:58 +0100

    (+) not refreshing stats api on window focus

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,5 +1,3 @@
-
-
 import {
   QueryClient,
   QueryClientProvider,
--- packages/client/src/services/api/global/stats/useStatsApi.ts
+++ packages/client/src/services/api/global/stats/useStatsApi.ts
@@ -2,5 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchStats } from './statsApi';
 
 export const useStats = () => {
-    return useQuery({queryKey: ['stats'], queryFn: fetchStats, select: (data) => data.data});
+    return useQuery({
+        queryKey: ['stats'],
+        queryFn: fetchStats,
+        select: (data) => data.data,
+        refetchOnWindowFocus: false
+    });
 };
