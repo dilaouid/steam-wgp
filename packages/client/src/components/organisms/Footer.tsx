ab55815d2702941fd8f8838eef24ee26b7f10715
dilaouid <dilaouid@student.42.fr>
2024-03-16 11:07:25 +0100

    (+) flags to select language in footer

--- packages/client/src/components/organisms/Footer.tsx
+++ packages/client/src/components/organisms/Footer.tsx
@@ -3,8 +3,20 @@ import { GithubIcon } from "../atoms/icons/footer/GithubIcon";
 import { CashIcon } from "../atoms/icons/footer/CashIcon";
 import { useTranslation } from "react-i18next";
 
+const flags = {
+    fr: 'https://flagicons.lipis.dev/flags/4x3/fr.svg',
+    en: 'https://flagicons.lipis.dev/flags/4x3/gb.svg',
+    es: 'https://flagicons.lipis.dev/flags/4x3/es.svg',
+    ja: 'https://flagicons.lipis.dev/flags/4x3/jp.svg',
+    de: 'https://flagicons.lipis.dev/flags/4x3/de.svg',
+};
+
 export const Footer: React.FC = () => {
-    const { t } = useTranslation('global/footer');
+    const { t, i18n } = useTranslation('global/footer');
+
+    const changeLanguage = (languageCode: string) => {
+        i18n.changeLanguage(languageCode);
+    };
 
     return(<><hr />
         <footer className="text-center">
@@ -24,6 +36,17 @@ export const Footer: React.FC = () => {
                     <a target='_blank' className="link-secondary" href="https://github.com/dilaouid/steam-wgp"><GithubIcon /></a>
                     <a target='_blank' className="link-secondary" href="https://ko-fi.com/dilaouid"><CashIcon /></a>
                 </ul>
+                <div className="flags-container">
+                        {Object.entries(flags).map(([code, url]) => (
+                            <img
+                                key={code}
+                                src={url}
+                                alt={code}
+                                style={{ opacity: i18n.resolvedLanguage === code ? 1 : 0.5, cursor: 'pointer', width: 24, height: 24, margin: 4, filter: i18n.resolvedLanguage === code ? 'grayscale(0)' : 'grayscale(.5)'}}
+                                onClick={() => changeLanguage(code)}
+                            />
+                        ))}
+                    </div>
                 <p className="mb-0">{ t('copy') }</p>
             </div>
         </footer>
