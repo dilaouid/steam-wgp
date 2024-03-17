fdfac66e58e2c37c98f11c3084ff9a017bdc12b5
dilaouid <dilaouid@student.42.fr>
2024-03-17 10:56:13 +0100

    (+) left column for login page

--- packages/client/src/components/templates/Loginpage.tsx
+++ packages/client/src/components/templates/Loginpage.tsx
@@ -1,3 +1,26 @@
+import styled from 'styled-components'
+import { Container, Row } from 'react-bootstrap';
+
+import BackgroundImage from '../../assets/images/loginpage/bg.png'
+import { LeftColumnLogin } from '../organisms/login/LeftColumnLogin';
+
+const LoginSection = styled.section`
+    background: url(${BackgroundImage}) right / cover;
+    padding-top: 9px;
+    padding-bottom: 27px;
+`;
+
+const LoginContainer = styled(Container)`
+    margin-top: 21px;
+`;
+
 export const Loginpage = () => {
-    return (<></>)
+    return (
+    <LoginSection>
+        <LoginContainer>
+            <Row>
+                <LeftColumnLogin />
+            </Row>
+        </LoginContainer>
+    </LoginSection>)
 }
--- packages/client/src/types/IUser.ts
+++ packages/client/src/types/IUser.ts
@@ -1,5 +1,5 @@
 export interface IUser {
     username: string;
     avatar_hash: string;
-    steamderId: string;
+    steamderId: string | null;
 }
