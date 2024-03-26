b54e7247d35c63be0238e2b6b43e571e947c90ed
dilaouid <dilaouid@student.42.fr>
2024-03-26 11:45:37 +0100

    (+) auth api (check auth)

--- packages/client_old/src/utils/getCookie.ts
+++ packages/client_old/src/utils/getCookie.ts
@@ -1,7 +1,7 @@
 export const getCookieValue = (name: string): string | undefined => {
-    const matches = document.cookie.match(new RegExp(
-      // eslint-disable-next-line no-useless-escape
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
+  const matches = document.cookie.match(new RegExp(
+    // eslint-disable-next-line no-useless-escape
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
