043e8425f08ad24f36b421689a752eb43e3784a6
dilaouid <dilaouid@student.42.fr>
2024-03-28 12:14:47 +0100

    (+) redirect after login successful

--- packages/client/src/components/organisms/login/RightColumnLogin.tsx
+++ packages/client/src/components/organisms/login/RightColumnLogin.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { Col, ProgressBar } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "@tanstack/react-router";
 import useSSEQuery from "../../../services/sse/loginSSE";
 import { ProgressLabelComponent } from "../../atoms/login/ProgressLabel";
+import { useEffect } from "react";
 
 const RightBlock = styled.div`
     background: #060606d2;
@@ -22,6 +24,16 @@ const Progress = styled(ProgressBar)`
 export const RightColumnLogin: React.FC = () => {
     const { t } = useTranslation('pages/login');
     const { data } = useSSEQuery();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (data[data.length - 1]?.complete && data[data.length - 1]?.type === 'success') {
+            // wait 3000ms before navigating to the home page
+            setTimeout(() => {
+                navigate({ to: '/' });
+            }, 3000);
+        }
+    }, [data, navigate]);
 
     return (
         <Col className="col-12 col-lg-8 text-center" data-aos="zoom-out" data-aos-duration="600">
