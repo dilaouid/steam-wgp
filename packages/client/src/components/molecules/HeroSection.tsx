c3e9b064aad7478d04e8a3849c3f591b8361f49c
dilaouid <dilaouid@student.42.fr>
2024-03-29 16:07:40 +0100

    (+) change hero section if user is authenticated

--- packages/client/src/components/molecules/HeroSection.tsx
+++ packages/client/src/components/molecules/HeroSection.tsx
@@ -1,5 +1,6 @@
 import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
+import { useAuthStore } from '../../store/authStore';
 
 const Section = styled.div`
     max-width: 522px;
@@ -14,9 +15,11 @@ const PinkTextWhiteBackground = styled.span`
     color: rgb(231, 19, 210);
     background-color: rgb(255, 255, 255);
 `;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const HeroSection = () => {
     const { t } = useTranslation('pages/homepage');
+    const { isAuthenticated } = useAuthStore();
 
     return (<Section>
         <h1 className="text-uppercase fw-bold" data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
@@ -25,10 +28,10 @@ export const HeroSection = () => {
         <p className="my-3">
             <Trans t={t} i18nKey="hero.paragraph" components={{ 1: <PinkText /> }} />
         </p>
-        <a className="btn btn-info btn-lg me-2" role="button" href="/login">
+        { !isAuthenticated && <a className="btn btn-info btn-lg me-2" role="button" href={ BASE_URL + "/auth/steam" }>
             {t('hero.login')}
-        </a>
-        <a className="btn btn-outline-info btn-lg" role="button" href="/steamders">
+        </a> }
+        <a className={`btn ${isAuthenticated ? 'btn-info' : 'btn-outline-info'} btn-lg`} role="button" href="/steamders">
             <Trans t={t} i18nKey="hero.steamders" components={{ 1: <strong /> }} />
         </a>
     </Section>);
