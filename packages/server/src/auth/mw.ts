e0de35fa79c6e14c512e09f697800b4c3489b91c
dilaouid <dilaouid@student.42.fr>
2024-04-01 09:21:49 +0200

    (+) create mw to allow unauthenticated user in some routes

--- packages/server/src/auth/mw.ts
+++ packages/server/src/auth/mw.ts
@@ -26,3 +26,29 @@ export async function isAuthenticated(req: FastifyRequest, res: FastifyReply) {
     return res.status(401).send({ error: 'Forbidden' });
   }
 }
+
+export async function allowUnauthenticated(req: FastifyRequest, res: FastifyReply) {
+  try {
+    let token;
+
+    if (req.cookies && req.cookies.token)
+      token = req.cookies.token;
+    else if (req.headers.authorization) {
+      const authHeader = req.headers.authorization;
+      const bearerToken = authHeader.split(' ');
+      if (bearerToken.length === 2 && bearerToken[0] === 'Bearer') {
+        token = bearerToken[1];
+      }
+    }
+
+    if (!token) {
+      return;
+    }
+
+    const secretKey = req.server.config.SECRET_KEY;
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+  } catch (error) {
+    return res.status(401).send({ error: 'Forbidden' });
+  }
+}
