f04ee1023e00aa8ee10c68511f658a40dc74d69e
dilaouid <dilaouid@student.42.fr>
2024-04-03 21:03:27 +0200

    [C] (+) fix redirect for steamder page

--- packages/client/src/routes/steamder/$steamderId.tsx
+++ packages/client/src/routes/steamder/$steamderId.tsx
@@ -2,21 +2,44 @@ import { useEffect } from 'react';
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 import { getSteamder } from '../../services/api/waitlists/get';
+import { useAuthStore } from '../../store/authStore';
+import { useSteamderStore } from '../../store/steamderStore';
 
 import { Steamderpage } from '../../components/templates/Steamder_page';
-import { Loader } from '../../components/atoms/Loader';
 
 import AOS from 'aos';
 
+
+const getIsAuthenticated = () => {
+  return useAuthStore.getState().isAuthenticated;
+}
+
 export const Route = createFileRoute("/steamder/$steamderId")({
   component: Steamder,
-  loader: ({ params: { steamderId } }) => getSteamder(steamderId),
-  beforeLoad: () => <Loader />,
+  beforeLoad: ({ params: { steamderId } }) => {
+    const isAuthenticated = useAuthStore.getState().isAuthenticated;
+    if (!isAuthenticated) {
+      localStorage.setItem('postLoginRedirect', '/steamder/' + steamderId);
+      window.location.href = `${import.meta.env.VITE_BASE_URL}/auth/steam`;
+      return;
+    }
+
+  },
+  loader: ({ params: { steamderId } }) => {
+    const { setSteamder } = useSteamderStore.getState();
+
+    getSteamder(steamderId).then((steamder) => {
+      setSteamder(steamder.data);
+    })
+  },
   onError: () => {
-    throw redirect({
-      to: '/'
-    });
-  }
+    if (getIsAuthenticated()) {
+      throw redirect({
+        to: '/'
+      });
+    }
+  },
+  staleTime: 1000 * 60 * 5
 })
 
 function Steamder() {
