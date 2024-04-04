ebc03221776ea07229b7cf48b9e1772f17b3aefb
dilaouid <dilaouid@student.42.fr>
2024-04-04 10:32:41 +0200

    [C] (+) find mismatched players utils fn

---
+++ packages/client/src/utils/findMismatchedPlayers.ts
@@ -0,0 +1,25 @@
+import { ISteamder } from "../types/ISteamder";
+
+interface IMismatchedPlayers {
+    player1: string;
+    player2: string;
+}
+
+export const findMismatchedPlayers = (steamder: ISteamder): IMismatchedPlayers[] => {
+    if (!steamder || steamder.players.length < 2) return [];
+
+    const mismatchMessages: IMismatchedPlayers[] = [];
+
+    for (let i = 0; i < steamder.players.length; i++) {
+        for (let j = i + 1; j < steamder.players.length; j++) {
+            const player1 = steamder.players[i];
+            const player2 = steamder.players[j];
+
+            const commonGames = player1.games.filter(game => player2.games.includes(game));
+            if (commonGames.length === 0) {
+                mismatchMessages.push({player1: player1.username, player2: player2.username});
+            }
+        }
+    }
+    return mismatchMessages;
+};
