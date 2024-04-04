4fbb95901162b169cd2e05feab81f65de962f708
dilaouid <dilaouid@student.42.fr>
2024-04-04 10:18:34 +0200

    [C] (+) template atomic design steamder page

--- packages/client/src/components/templates/Steamder_page.tsx
+++ packages/client/src/components/templates/Steamder_page.tsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+
+import CoverImage from '../../assets/images/steamderpage/cover.jpg';
+import { Header } from "../molecules/steamder/Header";
+import { RoomPlayers } from "../molecules/steamder/RoomPlayers";
+import { RoomInformations } from "../molecules/steamder/RoomInformations";
+
+const StyledSection = styled.section`
+    padding-top: 9px;
+    padding-bottom: 27px;
+    background-attachment: fixed;
+    background-image: url(${CoverImage});
+    background-size: cover;
+    background-position: right;
+`;
+
 export const Steamderpage = () => {
     return (
-        <>
-        </>)
+        <StyledSection>
+            <Header />
+            <RoomPlayers />
+            <RoomInformations />
+        </StyledSection>
+    );
 };
