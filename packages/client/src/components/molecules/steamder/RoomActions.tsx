26bdb8e7f6fb85d2eb4c7c787dcb9b824485e998
dilaouid <dilaouid@student.42.fr>
2024-04-16 22:59:46 +0200

    [C] (+) leave room inside steamder

--- packages/client/src/components/molecules/steamder/RoomActions.tsx
+++ packages/client/src/components/molecules/steamder/RoomActions.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { useNavigate } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 
 import { Row, Col, Button } from "react-bootstrap";
 
 import { useSteamderStore } from "../../../store/steamderStore";
 import { useAuthStore } from "../../../store/authStore";
+import { useLeaveSteamder } from "../../../hooks/useLeaveSteamder";
 
 import { IPlayer } from "../../../types/ISteamder";
 
@@ -14,13 +17,29 @@ const StyledRow = styled(Row)`
 
 export const RoomActions: React.FC = () => {
     const { t } = useTranslation("pages/steamder", { keyPrefix: "waitlist.actions" });
-    const { steamder } = useSteamderStore();
-    const { user } = useAuthStore();
+    const { steamder, setSteamder } = useSteamderStore();
+    const { user, setUser } = useAuthStore();
+    const navigate = useNavigate();
+    const leaveMutation = useLeaveSteamder();
+
+    const [loading, setLoading] = useState(false);
 
     if (!steamder) return null;
 
     const admin: IPlayer = steamder.players?.find(p => p.player_id == steamder.admin_id) as IPlayer;
     const isAdmin = admin?.player_id == user?.id;
+
+    const handleLeave = () => {
+        setLoading(true);
+        leaveMutation.mutateAsync(steamder.id).then(() => {
+            if (!user) return;
+            setUser({ ...user, waitlist: null });
+            setSteamder(null);
+        }).finally(() => {
+            setLoading(false)
+            navigate({ to: "/steamders" });
+        });
+    };
 
     return (
         <StyledRow className="justify-content-center">
@@ -30,7 +49,7 @@ export const RoomActions: React.FC = () => {
                 }>{t('start')}</Button>
             </Col> }
             <Col sm={"auto"} className="align-self-center">
-                <Button variant="outline-danger" className="shadow">{t('leave')}</Button>
+                <Button variant="outline-danger" className="shadow" onClick={handleLeave} disabled={loading}>{t('leave')}</Button>
             </Col>
         </StyledRow>
     );
