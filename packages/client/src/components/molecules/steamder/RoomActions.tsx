d2615699da9a622abe1dc83c58e6db5360227bac
dilaouid <dilaouid@student.42.fr>
2024-04-19 12:25:39 +0200

    [C] (+) leave steamder confirm modal

--- packages/client/src/components/molecules/steamder/RoomActions.tsx
+++ packages/client/src/components/molecules/steamder/RoomActions.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button, Modal } from "react-bootstrap";
 
 import { useSteamderStore } from "../../../store/steamderStore";
 import { useAuthStore } from "../../../store/authStore";
@@ -24,13 +24,19 @@ export const RoomActions: React.FC = () => {
     const leaveMutation = useLeaveSteamder();
 
     const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
 
     if (!steamder) return null;
 
     const admin: IPlayer = steamder.players?.find(p => p.player_id == steamder.admin_id) as IPlayer;
     const isAdmin = admin?.player_id == user?.id;
 
-    const handleLeave = () => {
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        setShow(true);
+    };
+
+    const leaveRoom = () => {
         setLoading(true);
         leaveMutation.mutateAsync(steamder.id).then(() => {
             if (!user) return;
@@ -44,15 +50,34 @@ export const RoomActions: React.FC = () => {
     };
 
     return (
-        <StyledRow className="justify-content-center">
-            { isAdmin && <Col sm={"auto"} className="align-self-center">
-                <Button variant="outline-info" className="shadow" disabled={
-                    steamder.players?.length < 2 || (steamder.display_all_games && steamder.all_games == 0) || (!steamder.display_all_games && steamder.common_games == 0)
-                }>{t('start')}</Button>
-            </Col> }
-            <Col sm={"auto"} className="align-self-center">
-                <Button variant="outline-danger" className="shadow" onClick={handleLeave} disabled={loading}>{t('leave')}</Button>
-            </Col>
-        </StyledRow>
+        <>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{ t('modal.leave.title') }</Modal.Title>
+                </Modal.Header>
+                    <Modal.Body>
+                        { t('modal.leave.content') }
+                        { isAdmin && <p className="text-warning fw-bold"> { t('modal.leave.warning') } </p> }
+                    </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        { t('modal.leave.no') }
+                    </Button>
+                    <Button variant="warning" onClick={leaveRoom}>
+                        { t('modal.leave.yes') }
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <StyledRow className="justify-content-center">
+                { isAdmin && <Col sm={"auto"} className="align-self-center">
+                    <Button variant="outline-info" className="shadow" disabled={
+                        steamder.players?.length < 2 || (steamder.display_all_games && steamder.all_games == 0) || (!steamder.display_all_games && steamder.common_games == 0)
+                    }>{t('start')}</Button>
+                </Col> }
+                <Col sm={"auto"} className="align-self-center">
+                    <Button variant="outline-danger" className="shadow" onClick={handleShow} disabled={loading}>{t('leave')}</Button>
+                </Col>
+            </StyledRow>
+        </>
     );
 };
