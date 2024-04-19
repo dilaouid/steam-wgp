adbb5a9d34284933c001be154f7773773cc596c7
dilaouid <dilaouid@student.42.fr>
2024-04-19 18:59:53 +0200

    [C] (+) invalide steamders query on switch display games

--- packages/client/src/services/websocket/send/switchDisplayGames.ts
+++ packages/client/src/services/websocket/send/switchDisplayGames.ts
@@ -1,7 +1,7 @@
 import useWebSocketStore from "../../../store/websocketStore";
 
 export const switchDisplayGames = () => {
-    const { sendMessage } = useWebSocketStore.getState();    
+    const { sendMessage } = useWebSocketStore.getState();
     const data = JSON.stringify({ action: 'allGamesSwitch' });
     
     sendMessage(data);
