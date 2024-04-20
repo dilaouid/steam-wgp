114122903b7954b03a1999c34b18567107555a72
dilaouid <dilaouid@student.42.fr>
2024-04-21 00:32:19 +0200

    [C] (-) remove countdown draft

--- packages/client/src/components/templates/SteamderWait_page.tsx
+++ packages/client/src/components/templates/SteamderWait_page.tsx
@@ -4,7 +4,6 @@ import CoverImage from '../../assets/images/steamderpage/cover.jpg';
 import { Header } from "../molecules/steamder/Header";
 import { RoomPlayers } from "../molecules/steamder/RoomPlayers";
 import { RoomInformations } from "../molecules/steamder/RoomInformations";
-import Countdown, { zeroPad } from "react-countdown";
 
 const StyledSection = styled.section`
     padding-top: 9px;
@@ -19,11 +18,6 @@ const StyledSection = styled.section`
 export const SteamderWaitPage = () => {
     return (
         <StyledSection>
-            <Countdown date={Date.now() + 500000} daysInHours={true} renderer={({ minutes, seconds }) => 
-                <span>
-                    { zeroPad(minutes) }:{ zeroPad(seconds) }
-                </span>
-            } />
             <Header />
             <RoomPlayers />
             <RoomInformations />
