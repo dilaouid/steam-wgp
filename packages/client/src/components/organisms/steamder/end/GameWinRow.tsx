ed928b34af5d4d063bf737db803bae0f55f3a5ab
dilaouid <dilaouid@student.42.fr>
2024-04-22 12:57:28 +0200

    [C] (+) link to shop page in result steamder

--- packages/client/src/components/organisms/steamder/end/GameWinRow.tsx
+++ packages/client/src/components/organisms/steamder/end/GameWinRow.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import { Row } from "react-bootstrap";
 import { WinnerColumn } from "../../../molecules/steamder/end/WinnerColumn";
+import { ShopPageColumn } from "../../../molecules/steamder/end/ShopPageColumn";
 
 const StyledRow = styled(Row)`
     margin-bottom: 17px;
@@ -11,6 +12,7 @@ export const GameWinRow = () => {
     return (
         <StyledRow>
             <WinnerColumn />
+            <ShopPageColumn />
         </StyledRow>
     )
 };
