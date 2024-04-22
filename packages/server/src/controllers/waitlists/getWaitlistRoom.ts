ab3b7be6255ad302ab76de8dd3e4b3a9be2ff85c
dilaouid <dilaouid@student.42.fr>
2024-04-22 22:27:31 +0200

    [S] (-) take out i18n in some API response

--- packages/server/src/controllers/waitlists/getWaitlistRoom.ts
+++ packages/server/src/controllers/waitlists/getWaitlistRoom.ts
@@ -3,7 +3,6 @@ import { Player } from "../../models/Players";
 import { getWaitlist } from "../../models/Waitlists";
 import { isAuthenticated } from "../../auth/mw";
 import { APIResponse } from "../../utils/response";
-import i18next from "../../plugins/i18n.plugin";
 
 export interface getWaitlistWithPlayersParams {
     id: string;
@@ -31,16 +30,16 @@ async function getWaitlistWithPlayers(request: FastifyRequest< { Params: getWait
   const player = request.user as Player;
 
   if (!id.trim())
-    return APIResponse(reply, null, i18next.t('no_id_provided', { lng: request.userLanguage }), 400);
+    return APIResponse(reply, null, 'no_id_provided', 400);
   try {
     const waitlist = await getWaitlist(fastify, id.trim(), BigInt(player.id));
     if (!waitlist) {
       fastify.log.warn(`Steamder ${id} not found`);
-      return APIResponse(reply, null, i18next.t('room_does_not_exist', { lng: request.userLanguage }), 404);
+      return APIResponse(reply, null, 'room_does_not_exist', 404);
     }
-    return APIResponse(reply, waitlist, i18next.t('retrieved_room', { lng: request.userLanguage }), 200);
+    return APIResponse(reply, waitlist, 'retrieved_room', 200);
   } catch (err) {
     fastify.log.error(err);
-    return APIResponse(reply, null, i18next.t('internal_server_error', { lng: request.userLanguage }), 500);
+    return APIResponse(reply, null, 'internal_server_error', 500);
   }
 }
