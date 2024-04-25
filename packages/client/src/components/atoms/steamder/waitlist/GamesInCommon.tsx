035df9e351e01863580a92b1b4589510a0893a91
dilaouid <dilaouid@student.42.fr>
2024-04-25 13:30:49 +0200

    [C] (+) fix forced fr text

--- packages/client/src/components/atoms/steamder/waitlist/GamesInCommon.tsx
+++ packages/client/src/components/atoms/steamder/waitlist/GamesInCommon.tsx
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 import { Tooltip, OverlayTrigger } from "react-bootstrap";
 import { IoGameController } from "react-icons/io5";
+import { useTranslation } from "react-i18next";
 
 const StyledText = styled.p`
     margin-top: 7px;
     font-size: 17px;
 `;
 
-const LabelTooltip = (message: number) => 
+const LabelTooltip = (message: string) => 
     <Tooltip id="common-games-tooltip">
-        { message } jeux en communs
+        { message }
     </Tooltip>;
 
 export const GamesInCommon: React.FC<{commonGames: number}> = ({ commonGames }) => {
+    const { t } = useTranslation('pages/steamder', { keyPrefix: 'waitlist.players' });
+
     return (
-        <OverlayTrigger placement="bottom" overlay={LabelTooltip(commonGames)}>
+        <OverlayTrigger placement="bottom" overlay={LabelTooltip(t('games_in_common', { count: commonGames }))}>
             <StyledText className={`lead text-center ${commonGames > 0 ? 'text-info' : 'text-danger'} user-select-none`}>
                 <strong> { commonGames } </strong>
                 <IoGameController />
