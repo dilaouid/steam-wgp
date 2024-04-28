b739901fadb608f51dcf6b1a96eb2fe21082465e
dilaouid <dilaouid@student.42.fr>
2024-04-28 19:33:41 +0200

    [C] (+) add podium in homepage

--- packages/client/src/components/templates/Home_page.tsx
+++ packages/client/src/components/templates/Home_page.tsx
@@ -1,6 +1,7 @@
 import { FeaturesHome } from "../organisms/homepage/FeaturesHome"
 import { HowTo } from "../organisms/homepage/HowTo"
 import { ParallaxBackgroundHome } from "../organisms/homepage/ParallaxBackgroundHome"
+import { Podium } from "../organisms/homepage/Podium"
 import { StatsHome } from "../organisms/homepage/StatsHome"
 
 export const Homepage = () => {
@@ -9,6 +10,7 @@ export const Homepage = () => {
         <ParallaxBackgroundHome />
         <FeaturesHome />
         <StatsHome />
+        <Podium />
         <HowTo />
         <hr />
     </div>)
