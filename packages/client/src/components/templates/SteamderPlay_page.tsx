18ae3ae01d4fd751dd7d403d34a243d18cce2171
dilaouid <dilaouid@student.42.fr>
2024-04-28 12:48:33 +0200

    [C] (+) shuffle games in start of steamder

--- packages/client/src/components/templates/SteamderPlay_page.tsx
+++ packages/client/src/components/templates/SteamderPlay_page.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
+
 import styled from "styled-components";
 
 import CoverImage from '../../assets/images/steamderpage/cover.jpg';
 import { Header } from "../molecules/steamder/game/Header";
 import { GameRow } from "../organisms/steamder/game/GameRow";
+
+import { useSteamderStore } from "../../store/steamderStore";
 
 const StyledSection = styled.section`
     padding-top: 9px;
@@ -15,6 +19,16 @@ const StyledSection = styled.section`
 `;
 
 export const SteamderPlayPage = () => {
+    const { steamder, setSteamder } = useSteamderStore();
+
+    useEffect(() => {
+        if (steamder) {
+            const property = steamder.display_all_games ? 'all_games' : 'common_games';
+            setSteamder({ ...steamder, [property]: steamder[property].sort(() => Math.random() - 0.5) });
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <StyledSection>
             <Header />
