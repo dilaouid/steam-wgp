51235ff6f125e8053e36f74f1e5fb128b13291c2
dilaouid <dilaouid@student.42.fr>
2024-04-30 16:08:07 +0200

    [C] (+) alert user about clean libraries

--- packages/client/src/components/wrappers/AuthWrapper.tsx
+++ packages/client/src/components/wrappers/AuthWrapper.tsx
@@ -1,15 +1,22 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useCheckAuth } from "../../hooks/useCheckAuth";
 import { useAuthStore } from "../../store/authStore";
 import { Loader } from "../atoms/Loader";
+import { Modal, Button } from "react-bootstrap";
+import { Trans, useTranslation } from "react-i18next";
+
 
 type AuthWrapperProps = {
     children: ReactNode;
 };
 
 export const AuthWrapper: React.FC<AuthWrapperProps> = ({ children }) => {
+    const [ show, setShow ] = useState<boolean>(true);
     const { data, isError, error, isSuccess, isFetched } = useCheckAuth();
     const { setUser, toggleAuth } = useAuthStore();
+    const { t } = useTranslation('global/information', { keyPrefix: 'reset' });
+
+    const isAlertedLibrary = localStorage.getItem('alertedLibrary');
 
     useEffect(() => {
         if (isError) {
@@ -24,5 +31,22 @@ export const AuthWrapper: React.FC<AuthWrapperProps> = ({ children }) => {
     if (!isFetched)
         return <Loader />;
 
-    return <>{children}</>;
+    return <>
+        { !isAlertedLibrary &&
+            <Modal show={show} onHide={() => localStorage.setItem('alertedLibrary', 'true')} centered animation>
+                <Modal.Header>
+                    <Modal.Title>{ t('header') }</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Trans t={t} i18nKey="body" components={{ 1: <a className="text-decoration-none" href="https://twitter.com/LaouidD/status/1785045680295235762" target="_blank" />, 2: <strong className="fw-bold text-warning" />, 3: <p /> }} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="info" onClick={() => {
+                        setShow(false);
+                        localStorage.setItem('alertedLibrary', 'true')
+                    }}> { t('button') }</Button>
+                </Modal.Footer>
+            </Modal> }
+        { children }
+    </>;
 };
