f5f5e225d57d9ec93f84d60fb41dc9dea2df3e2b
dilaouid <dilaouid@student.42.fr>
2024-09-16 19:53:40 +0200

    [S] (^) change games schema (remove serial)

--- packages/server/src/infrastructure/data/schemas/games.ts
+++ packages/server/src/infrastructure/data/schemas/games.ts
@@ -1,6 +1,6 @@
-import { pgTable, serial, boolean } from 'drizzle-orm/pg-core';
+import { pgTable, integer, boolean } from 'drizzle-orm/pg-core';
 
 export const games = pgTable('games', {
-  id: serial('id').primaryKey(),
+  id: integer('id').primaryKey(),
   is_selectable: boolean('is_selectable').default(false)
 });
