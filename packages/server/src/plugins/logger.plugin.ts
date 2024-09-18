395d3c2de215b98b25b00612295e088952e61b54
dilaouid <dilaouid@student.42.fr>
2024-09-18 16:18:02 +0200

    add logs folder if not exists

--- packages/server/src/plugins/logger.plugin.ts
+++ packages/server/src/plugins/logger.plugin.ts
@@ -1,4 +1,12 @@
 import pino from 'pino';
+import fs from 'fs';
+import path from 'path';
+
+const logsDir = path.resolve('./logs');
+
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
 
 export const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
