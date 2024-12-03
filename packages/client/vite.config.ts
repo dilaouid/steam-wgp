26f6d8d9102ab44b502531f25b7c13623404c330
dilaouid <dilaouid@student.42.fr>
2024-12-03 19:55:30 +0100

    [C] (+) vite config + i18n debug boolean value to production/development mode

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -9,6 +11,14 @@ export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './public/locales',
+          dest: ''
+        }
+      ]
+    })
   ],
 })
