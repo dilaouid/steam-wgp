b686306bc98563f70bb3fc2f144e6a1378f7ece4
dilaouid <dilaouid@student.42.fr>
2024-12-04 07:15:50 +0100

    [C] (+) improve UX steamder creation (functional disabled button)

--- packages/client/src/services/api/steamders/create.ts
+++ packages/client/src/services/api/steamders/create.ts
@@ -2,19 +2,20 @@ import { getCookieValue } from "@utils/cookieUtils";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const createSteamder = async ({name, isPrivate}: { name: string, isPrivate: boolean }) => {
-    try {
-        const token = getCookieValue('token');
-        return fetch(`${BASE_URL}/steamder`, {
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            credentials: "include",
-            body: JSON.stringify({ name, isPrivate })
-        }).then(res => res.json());
-    } catch (err) {
-        console.error("Erreur lors de la création de la Steamder :", err);
-        throw new Error(err as string);
+    const token = getCookieValue('token');
+    const response = await fetch(`${BASE_URL}/steamder`, {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        credentials: "include",
+        body: JSON.stringify({ name, isPrivate })
+    })
+
+    if (!response.ok) {
+        const errorDetails = await response.json();
+        throw new Error(`Erreur lors de la création de la Steamder: HTTP error! Status: ${response.status}`, { cause: errorDetails });
     }
+    return response.json();
 };
