ed5529629c2d1a3b70c6f6fa60895b897cdae4b6
dilaouid <dilaouid@student.42.fr>
2025-01-16 19:29:20 +0100

    [B] (+) init structure project for admin dashboard

---
+++ packages/dashboard/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './public/locales',
+          dest: ''
+        }
+      ]
+    })
+  ],
+})
--- packages/server/src/infrastructure/data/schemas/players.ts
+++ packages/server/src/infrastructure/data/schemas/players.ts
@@ -1,8 +1,9 @@
-import { pgTable, bigint, varchar } from "drizzle-orm/pg-core";
+import { pgTable, bigint, varchar, boolean } from "drizzle-orm/pg-core";
 
 export const players = pgTable("players", {
   id: bigint("id", { mode: "bigint" }).primaryKey(),
   avatar_hash: varchar("avatar_hash", { length: 255 }).unique(),
   username: varchar("username", { length: 255 }),
-  profileurl: varchar("profileurl", { length: 255 }).unique()
+  profileurl: varchar("profileurl", { length: 255 }).unique(),
+  isAdmin: boolean("admin").default(false),
 });
