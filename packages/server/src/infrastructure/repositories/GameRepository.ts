dc0a1248b2b6a091f2acfc35f0c51559d1e807b3
dilaouid <dilaouid@student.42.fr>
2025-01-20 19:56:19 +0100

    [S] (~/+) fix repository architecture - remove Dashboard folder

--- packages/server/src/infrastructure/repositories/GameRepository.ts
+++ packages/server/src/infrastructure/repositories/GameRepository.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { games } from "@schemas";
-import { count, eq, inArray } from "drizzle-orm";
+import { and, count, eq, inArray } from "drizzle-orm";
 
 export const countGames = async (fastify: FastifyInstance): Promise<any> => {
   const { db } = fastify;
@@ -61,4 +61,88 @@ export const getGameById = async (
 ): Promise<any> => {
   const { db } = fastify;
   return db.select().from(games).where(eq(games.id, id)).execute();
+};
+
+/**
+ * Create a new game.
+ *
+ * @param {FastifyInstance} fastify - The Fastify instance.
+ * @param {int} id - The game Steam id.
+ * @param {boolean} isSelectable - Whether the game is selectable in a Steamder
+ * @returns {Promise<any>} - A promise that resolves to the created game.
+ */
+export const createGame = async (
+  fastify: FastifyInstance,
+  id: number,
+  is_selectable: boolean
+): Promise<any> => {
+  try {
+    const { db } = fastify;
+    return db.insert(games).values({ id, is_selectable }).returning().execute();
+  } catch (err) {
+    fastify.log.error(err);
+    throw new Error("Failed to create game");
+  }
+};
+
+/**
+ * Get a list of games according to the search query.
+ *
+ * @param {FastifyInstance} fastify - The Fastify instance.
+ * @param {string} search - The search query.
+ * @param {number} page - The page number.
+ * @param {number} limit - The number of games per page.
+ * @returns {Promise<any[]>} - A promise that resolves to the list of games.
+ */
+export const searchGames = async (
+  fastify: FastifyInstance,
+  onlyIsSelectable: boolean,
+  onlyNotSelectable: boolean,
+  limit: number,
+  offset: number
+): Promise<any[]> => {
+  try {
+    const { db } = fastify;
+    return db
+      .select()
+      .from(games)
+      .where(
+        and(
+          eq(games.is_selectable, onlyIsSelectable),
+          eq(games.is_selectable, onlyNotSelectable)
+        )
+      )
+      .limit(limit)
+      .offset(offset);
+  } catch (err) {
+    fastify.log.error(err);
+    throw new Error("Failed to search games");
+  }
+};
+
+/**
+ * Update a game by its id.
+ *
+ * @param {FastifyInstance} fastify - The Fastify instance.
+ * @param {int} id - The game Steam id.
+ * @param {boolean} isSelectable - Whether the game is selectable in a Steamder
+ * @returns {Promise<any>} - A promise that resolves to the updated game.
+ */
+export const updateGame = async (
+  fastify: FastifyInstance,
+  id: number,
+  is_selectable: boolean
+): Promise<any> => {
+  try {
+    const { db } = fastify;
+    return db
+      .update(games)
+      .set({ is_selectable })
+      .where(eq(games.id, id))
+      .returning()
+      .execute();
+  } catch (err) {
+    fastify.log.error(err);
+    throw new Error("Failed to update game");
+  }
 };
