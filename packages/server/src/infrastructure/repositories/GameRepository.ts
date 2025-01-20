1fe4ccac432a0c950c01d7e7ec65e4652706d873
dilaouid <dilaouid@student.42.fr>
2025-01-20 19:08:18 +0100

    [S] (+) new method for game repository to get a specific game by its ID

--- packages/server/src/infrastructure/repositories/GameRepository.ts
+++ packages/server/src/infrastructure/repositories/GameRepository.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { games } from "@schemas";
-import { count, inArray } from "drizzle-orm";
+import { count, eq, inArray } from "drizzle-orm";
 
 export const countGames = async (fastify: FastifyInstance): Promise<any> => {
   const { db } = fastify;
@@ -42,8 +42,23 @@ export const getGamesAccordingToList = async (
  */
 export const insertGames = async (
   fastify: FastifyInstance,
-  gamesToAdd: { id: number; is_selectable: boolean; }[]
+  gamesToAdd: { id: number; is_selectable: boolean }[]
 ): Promise<any> => {
   const { db } = fastify;
   return db.insert(games).values(gamesToAdd).returning().execute();
+};
+
+/**
+ * Get a game by its ID.
+ *
+ * @param {FastifyInstance} fastify - The Fastify instance.
+ * @param {number} id - The ID of the game.
+ * @returns {Promise<any>} - A promise that resolves to the list of games.
+ */
+export const getGameById = async (
+  fastify: FastifyInstance,
+  id: number
+): Promise<any> => {
+  const { db } = fastify;
+  return db.select().from(games).where(eq(games.id, id)).execute();
 };
