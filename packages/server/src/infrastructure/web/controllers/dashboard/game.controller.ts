48359a69ce05773b23eb25bc86b01660e1d68bc7
dilaouid <dilaouid@student.42.fr>
2025-01-21 19:26:36 +0100

    [S] (+) game.controller+new syntax for writing controllers

---
+++ packages/server/src/infrastructure/web/controllers/dashboard/game.controller.ts
@@ -0,0 +1,59 @@
+import { addGame, getGame, paginateGames, updateGameStatus } from "@services/game.service";
+import { gamePropertiesSchema, isValidGameId, isValidSelectable, paginateGamesSchema } from "@validations/dashboard";
+import { createController } from "@utils/controller";
+
+export const gamesController = {
+  /*
+    * Get list of all games with pagination
+  */
+  getGames: createController(async ({ fastify, query }) => {
+    const { offset, limit, onlyIsSelectable, onlyNotSelectable } = paginateGamesSchema.parse(query);
+    const data = await paginateGames(fastify, offset, limit, { onlyIsSelectable, onlyNotSelectable });
+    return {
+      statusCode: 200,
+      data,
+      message: "OK"
+    };
+  }),
+
+  /*
+    * Get a game by its ID
+  */
+  getGame: createController(async ({ fastify, params }) => {
+    const { id } = params;
+    const data = await getGame(fastify, id);
+    return {
+      statusCode: 200,
+      data,
+      message: "OK"
+    };
+  }),
+
+  /*
+    * Create a game
+  */
+  createGame: createController(async ({ fastify, body }) => {
+    const parsedBody = gamePropertiesSchema.parse(body);
+    const data = await addGame(fastify, parsedBody);
+    return {
+      statusCode: 201,
+      data,
+      message: "game_added"
+    };
+  }),
+
+  /*
+    * Update a game by its ID (switching isSelectable)
+  */
+  updateGame: createController(async ({ fastify, params }) => {
+    const { id } = isValidGameId.parse(params);
+    const { is_selectable } = isValidSelectable.parse(params);
+
+    const data = await updateGameStatus(fastify, { id, is_selectable });
+    return {
+      statusCode: 200,
+      data,
+      message: "game_updated"
+    };
+  })
+}
