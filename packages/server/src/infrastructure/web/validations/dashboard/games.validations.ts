ff07fb0e31b4b1605a1d121cdff0ea5367aa17cd
dilaouid <dilaouid@student.42.fr>
2025-01-21 19:24:42 +0100

    [S] (+) changes games schemas validations

---
+++ packages/server/src/infrastructure/web/validations/dashboard/games.validations.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const paginateGamesSchema = z.object({
+  offset: z.coerce
+    .number()
+    .min(0, { message: "invalid_offset" })
+    .max(1000, { message: "invalid_offset" }),
+  limit: z.coerce
+    .number()
+    .min(1, { message: "invalid_limit" })
+    .max(100, { message: "invalid_limit" }),
+  onlyIsSelectable: z.coerce.boolean({ message: "invalid_boolean" }).optional(),
+  onlyNotSelectable: z.coerce.boolean({ message: "invalid_boolean" }).optional(),
+});
+
+export const gamePropertiesSchema = z.object({
+  id: z.coerce.number().min(1, { message: "invalid_id" }),
+  is_selectable: z.coerce.boolean({ message: "invalid_boolean" }).optional().default(false)
+});
+
+export const isValidGameId = z.object({
+  id: z.coerce.number().min(1, { message: "invalid_id" })
+});
+
+export const isValidSelectable = z.object({
+  is_selectable: z.coerce.boolean({ message: "invalid_boolean" }).optional().default(false)
+});
