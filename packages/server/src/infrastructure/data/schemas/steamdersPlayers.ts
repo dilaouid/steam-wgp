ac212855b89f54d3ed368be208cf742decde77b6
dilaouid <dilaouid@student.42.fr>
2025-01-22 14:44:55 +0100

    [S] (+) improve steamders_players schema

--- packages/server/src/infrastructure/data/schemas/steamdersPlayers.ts
+++ packages/server/src/infrastructure/data/schemas/steamdersPlayers.ts
@@ -1,9 +1,12 @@
-import { pgTable, primaryKey, bigint, uuid } from "drizzle-orm/pg-core";
+import { pgTable, primaryKey, bigint, uuid, timestamp, varchar } from "drizzle-orm/pg-core";
 import { players, steamders } from ".";
 
 export const steamdersPlayers = pgTable('steamders_players', {
   player_id: bigint('player_id', { mode: 'bigint'}).references(() => players.id, { onDelete: 'cascade' }),
-  steamder_id: uuid('steamder_id').references(() => steamders.id, { onDelete: 'cascade' })
+  steamder_id: uuid('steamder_id').references(() => steamders.id, { onDelete: 'cascade' }),
+  status: varchar('status', { enum: ['active', 'completed'] }).default('active'),
+  joined_at: timestamp('joined_at').defaultNow(),
+  completed_at: timestamp('completed_at')
 }, (steamder_player) => {
   return {
     pk: primaryKey({columns: [steamder_player.player_id, steamder_player.steamder_id]})
